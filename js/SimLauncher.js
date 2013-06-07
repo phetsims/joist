2127778f10190887b2ade6fa7065937004162ae4
Chris Malley <cmalley@pixelzoom.com>
2013-06-07 12:54:59 -0600

    Move string filenames to imageNames property of image loader object, update FAMB to reflect this change. Move BLL, Molarity and example-sim to PxLoader and SimLauncher.

--- js/SimLauncher.js
+++ js/SimLauncher.js
@@ -5,9 +5,8 @@
  *
  * @author Sam Reid
  */
-define( function( require ) {
+define( function() {
   'use strict';
-  var imageLoader = require( 'imageLoader' );
 
   return {
     /**
@@ -15,13 +14,17 @@ define( function( require ) {
      *
      * TODO: add an awesome loading screen
      *
-     * @param {String} imageNames the images to be loaded into the module system.
+     * @param {*} imageLoader an object with a {String|Array<String>} imageNames property
      * @param callback the callback function which should create and start the sim, given that the images are loaded
      */
-    launch: function( imageNames, callback ) {
+    launch: function( imageLoader, callback ) {
+
+      // load images and configure the image loader
       var loader = new PxLoader();
       var loadedImages = {};
-      imageNames.split( ' ' ).forEach( function( image ) {
+
+      var imageNamesArray = ( typeof imageLoader.imageNames === 'string' ) ? imageLoader.imageNames.split( ' ' ) : imageLoader.imageNames;
+      imageNamesArray.forEach( function( image ) {
         loadedImages[image] = loader.addImage( 'images/' + image );
       } );
       imageLoader.getImage = function( name ) { return loadedImages[name]; };
