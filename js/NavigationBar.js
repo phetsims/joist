62b0a1cef3e17c94231dd7113e7d8b6c4dab8f85
Sam Reid <reids@colorado.edu>
2013-06-23 22:23:29 -0600

    fixed jshint issue

--- js/NavigationBar.js
+++ js/NavigationBar.js
@@ -122,7 +122,7 @@ define( function( require ) {
     //TODO:   of the icon vs the text, if all of the buttons have the same dimensions.
     //TODO:   Currently this solves the simple case where all of the text is shorter than all of the icons (like in Build an Atom)
     var maxIconWidth = _.max( iconAndTextArray,function( i ) {return i.icon.width;} ).icon.width;
-    var maxButtonWidth = _.max( iconAndTextArray,function( i ) {return i.width} ).width;
+    var maxButtonWidth = _.max( iconAndTextArray,function( i ) {return i.width;} ).width;
     this.buttonHBox = new HBox( {spacing: maxIconWidth === maxButtonWidth ? 20 : 5, children: this.buttonArray} );
 
     //Add everything to the scene
