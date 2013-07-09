21ea2bd4d05b424d9829d212710a73883b697b44
Chris Malley <cmalley@pixelzoom.com>
2013-07-09 13:21:48 -0600

    cleanup

--- js/PhetMenu.js
+++ js/PhetMenu.js
@@ -1,14 +1,14 @@
 // Copyright 2002-2013, University of Colorado Boulder
 
 /**
- * Popup menu that is displayed when the user clicks on the bars in the bottom right in the navigation bar.
- * Would be nice to have a balloon triangle dropdown shape like in a comic book dialog.
+ * The "PhET" menu.
  *
  * @author Sam Reid
  */
 define( function( require ) {
   "use strict";
 
+  // imports
   var Node = require( 'SCENERY/nodes/Node' );
   var Shape = require( 'KITE/Shape' );
   var Path = require( 'SCENERY/nodes/Path' );
@@ -19,83 +19,97 @@ define( function( require ) {
   var ButtonListener = require( 'SCENERY/input/ButtonListener' );
   var log = require( 'AXON/log' );
 
+  // constants
   var FONT_SIZE = '18px';
+  var HIGHLIGHT_COLOR = '#a6d2f4';
 
+  // Creates a menu item that highlights and fires.
   var createMenuItem = function( text, width, height, callback ) {
 
-    var menuItem = new Node( { cursor: 'pointer' } );
+    var X_MARGIN = 5;
+    var Y_MARGIN = 3;
+    var CORNER_RADIUS = 5;
 
     var textNode = new Text( text, { fontSize: FONT_SIZE } );
-    var xMargin = 5;
-    var yMargin = 3;
-    var cornerRadius = 5;
-    var highlight = new Rectangle( 0, 0, width + xMargin + xMargin, height + yMargin + yMargin, cornerRadius, cornerRadius );
+    var highlight = new Rectangle( 0, 0, width + X_MARGIN + X_MARGIN, height + Y_MARGIN + Y_MARGIN, CORNER_RADIUS, CORNER_RADIUS );
 
+    var menuItem = new Node( { cursor: 'pointer' } );
     menuItem.addChild( highlight );
     menuItem.addChild( textNode );
 
-    textNode.left = highlight.left + xMargin;
+    textNode.left = highlight.left + X_MARGIN; // text is left aligned
     textNode.centerY = highlight.centerY;
 
-    menuItem.addInputListener( {enter: function() {
-      highlight.fill = '#a6d2f4';
-    }, exit: function() {
-      highlight.fill = null;
-    }} );
-
+    menuItem.addInputListener( {
+      enter: function() { highlight.fill = HIGHLIGHT_COLOR; },
+      exit: function() { highlight.fill = null; }
+    } );
     menuItem.addInputListener( new ButtonListener( {fire: callback } ) );
+
     return menuItem;
   };
 
   //TODO: The popup menu should scale with the size of the screen
   function PhetMenu( sim, options ) {
 
-    options = _.extend( {renderer: 'svg'}, options ); //TODO add default options
+    options = _.extend( {renderer: 'svg'}, options );
 
-    var simPopupMenu = this;
-    Node.call( this );
+    var thisMenu = this;
+    Node.call( thisMenu );
 
+    /*
+     * Description of the items in the menu. Each descriptor has these properties:
+     * {String} text - the item's text
+     * {Boolean} present - whether the item should be added to the menu
+     * {Function} callback - called when the item fires
+     */
     var itemDescriptors = [
       {
-        text: 'PhET Homepage', present: true, callback: function() {
-        window.open( "http://phet.colorado.edu" );
-        window.focus();
-      }},
+        text: 'PhET Homepage',
+        present: true,
+        callback: function() {
+          window.open( "http://phet.colorado.edu" );
+          window.focus();
+        }},
       {
-        text: 'Output Log', present: log.enabled ? true : false, callback: function() {
-        console.log( JSON.stringify( log.log ) );
-      }},
+        text: 'Output Log',
+        present: log.enabled ? true : false,
+        callback: function() {
+          console.log( JSON.stringify( log.log ) );
+        }},
       {
-        text: 'About...', present: true, callback: function() {
-        var aboutDialog = new AboutDialog( sim );
-        sim.addChild( aboutDialog );
-        var aboutDialogListener = {down: function() {
-          aboutDialog.removeInputListener( aboutDialogListener );
-          aboutDialog.detach();
-        }};
-        aboutDialog.addInputListener( aboutDialogListener );
-      }}
+        text: 'About...',
+        present: true,
+        callback: function() {
+          var aboutDialog = new AboutDialog( sim );
+          sim.addChild( aboutDialog );
+          var aboutDialogListener = {down: function() {
+            aboutDialog.removeInputListener( aboutDialogListener );
+            aboutDialog.detach();
+          }};
+          aboutDialog.addInputListener( aboutDialogListener );
+        }}
     ];
 
+    // Menu items have uniform size, so compute the max text dimensions.
     var keepItemDescriptors = _.filter( itemDescriptors, function( itemDescriptor ) {return itemDescriptor.present;} );
     var textNodes = _.map( keepItemDescriptors, function( item ) {return new Text( item.text, {fontSize: FONT_SIZE} );} );
+    var maxTextWidth = _.max( textNodes, function( node ) {return node.width;} ).width;
+    var maxTextHeight = _.max( textNodes, function( node ) {return node.height;} ).height;
 
-    //Compute bounds
-    var widestText = _.max( textNodes, function( node ) {return node.width;} );
-    var tallestText = _.max( textNodes, function( node ) {return node.height;} );
-
+    // Create the menu items.
     var items = _.map( itemDescriptors, function( itemDescriptor ) {
-      return createMenuItem( itemDescriptor.text, widestText.width, tallestText.height, itemDescriptor.callback );
+      return createMenuItem( itemDescriptor.text, maxTextWidth, maxTextHeight, itemDescriptor.callback );
     } );
 
+    // Create a comic-book-style bubble.
     var itemWidth = _.max( items,function( item ) {return item.width;} ).width;
     var itemHeight = _.max( items,function( item ) {return item.height;} ).height;
+    var X_MARGIN = 5;
+    var Y_MARGIN = 5;
 
-    var verticalSpacing = 0;
-    var padding = 5;
-    var bubbleWidth = itemWidth + padding * 2;
-    var bubbleHeight = itemHeight * items.length + padding * 2 + verticalSpacing * (items.length - 1);
-
+    var bubbleWidth = itemWidth + X_MARGIN + X_MARGIN;
+    var bubbleHeight = itemHeight * items.length + Y_MARGIN + Y_MARGIN;
     var bubble = new Rectangle( 0, 0, bubbleWidth, bubbleHeight, 8, 8, {fill: 'white', lineWidth: 1, stroke: 'black'} );
 
     var tail = new Shape();
@@ -103,30 +117,31 @@ define( function( require ) {
     tail.lineToRelative( 0, 20 );
     tail.lineToRelative( -20, -20 );
     tail.close();
-
-    this.addChild( bubble );
-    this.addChild( new Path( {shape: tail, fill: 'white'} ) );
+    thisMenu.addChild( bubble );
+    thisMenu.addChild( new Path( {shape: tail, fill: 'white'} ) );
 
     var tailOutline = new Shape();
     tailOutline.moveTo( bubbleWidth - 20, bubbleHeight );
     tailOutline.lineToRelative( 0, 20 - 2 );
     tailOutline.lineToRelative( -18, -18 );
-    this.addChild( new Path( {shape: tailOutline, stroke: 'black', lineWidth: 1} ) );
+    thisMenu.addChild( new Path( {shape: tailOutline, stroke: 'black', lineWidth: 1} ) );
 
-    var y = padding;
+    // Populate the bubble with menu items.
+    var y = Y_MARGIN;
     _.each( items, function( item ) {
       item.top = y;
-      item.left = padding;
-      simPopupMenu.addChild( item );
+      item.left = X_MARGIN;
+      thisMenu.addChild( item );
 
+      //TODO separators should be specified in itemDescriptors
       // Put a separator before the last item.
       if ( item === items[items.length - 2] ) {
-        simPopupMenu.addChild( new Path( {shape: Shape.lineSegment( 8, y + itemHeight + verticalSpacing / 2, bubbleWidth - 8, y + itemHeight + verticalSpacing / 2 ), stroke: 'gray', lineWidth: 1} ) );
+        thisMenu.addChild( new Path( {shape: Shape.lineSegment( 8, y + itemHeight, bubbleWidth - 8, y + itemHeight ), stroke: 'gray', lineWidth: 1} ) );
       }
-      y += itemHeight + verticalSpacing;
+      y += itemHeight;
     } );
 
-    this.mutate( options );
+    thisMenu.mutate( options );
   }
 
   inherit( Node, PhetMenu );
