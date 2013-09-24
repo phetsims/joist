65fc6a4739e0e782d91a2d7eff030e12567b2ce9
samreid <reids@colorado.edu>
2013-09-24 09:13:15 -0600

    Removed imageLoader argument from SimLauncher.launch

--- js/SimLauncher.js
+++ js/SimLauncher.js
@@ -15,10 +15,9 @@ define( function( require ) {
      *
      * TODO: add an awesome loading screen
      *
-     * @param {*} simImageLoader an object with an {Array<String>} imageNames property
      * @param callback the callback function which should create and start the sim, given that the images are loaded
      */
-    launch: function( simImageLoader, callback ) {
+    launch: function( callback ) {
 
       // image elements to remove once we are fully loaded
       var elementsToRemove = [];
@@ -34,36 +33,6 @@ define( function( require ) {
           callback();
         }
       }
-
-      //Load the images for a single imageLoader.
-      function load( imageLoader, path ) {
-        var loadedImages = {};
-        imageLoader.getImage = function( name ) { return loadedImages[name]; };
-
-        imageLoader.imageNames.forEach( function( image ) {
-          var filename = path + '/' + image;
-
-          // check to see if we have a reference to this image in the DOM (included with data URI in base64)
-          loadedImages[image] = document.getElementById( filename );
-          if ( loadedImages[image] ) {
-            // window.console && console.log && console.log( 'loaded ' + filename + ' with dimensions: ' + loadedImages[image].width + 'x' + loadedImages[image].height );
-            if ( loadedImages[image].width === 0 || loadedImages[image].height === 0 ) {
-              // if it exists but doesn't have dimensions, we wait until window's onload to trigger the "all images loaded" signal
-              delayCompletionEvent = true;
-            }
-
-            // mark the element to be removed from the DOM
-            elementsToRemove.push( loadedImages[image] );
-          }
-          else {
-            // TODO: only print warning if we detect we are a production / release candidate build
-            window.console && console.log && console.log( 'WARNING: could not find image: ' + filename + '.' );
-          }
-        } );
-      }
-
-      // load images and configure the image loader
-      load( simImageLoader, 'images' );
 
       // if image dimensions exist, immediately fire the "all images loaded" event
       if ( !delayCompletionEvent ) {
