97b4da253027055932d5b04fc7f9c41b6886bdd3
samreid <reids@colorado.edu>
2013-10-31 13:10:14 -0600

    Factored out screen size as a static constant

--- js/ScreenView.js
+++ js/ScreenView.js
@@ -16,6 +16,11 @@ define( function( require ) {
   var Path = require( 'SCENERY/nodes/Path' );
   var Shape = require( 'KITE/Shape' );
 
+  //Default to width and height for iPad2, iPad3, iPad4 running Safari with default tabs and decorations
+  //Simulations can change this to provide their own sizes or aspect ratios
+  //TODO: the code that uses these bounds needs to account for the minX and minY values if they are overriden in subclasses
+  var LAYOUT_BOUNDS = new Bounds2( 0, 0, 768, 504 );
+
   function ScreenView( options ) {
     Node.call( this, _.extend( {
       layerSplit: true // so we're not in the same layer as the navbar, etc.
@@ -29,36 +34,39 @@ define( function( require ) {
 
   inherit( Node, ScreenView, {
 
-    //Default to width and height for iPad2, iPad3, iPad4 running Safari with default tabs and decorations
-    //Simulations can change this to provide their own sizes or aspect ratios
-    //TODO: the code that uses these bounds needs to account for the minX and minY values if they are overriden in subclasses
-    layoutBounds: new Bounds2( 0, 0, 768, 504 ),
+      layoutBounds: LAYOUT_BOUNDS.copy(),
 
-    //Get the scale to use for laying out the sim components and the navigation bar, so its size will track with the sim size
-    getLayoutScale: function( width, height ) {
-      return Math.min( width / this.layoutBounds.width, height / this.layoutBounds.height );
+      //Get the scale to use for laying out the sim components and the navigation bar, so its size will track with the sim size
+      getLayoutScale: function( width, height ) {
+        return Math.min( width / this.layoutBounds.width, height / this.layoutBounds.height );
+      },
+
+      //Default layout function uses the layoutWidth and layoutHeight to scale the content (based on whichever is more limiting: width or height)
+      //and centers the content in the screen vertically and horizontally
+      //This function can be replaced by subclasses that wish to perform their own custom layout.
+      layout: function( width, height ) {
+        this.resetTransform();
+
+        var scale = this.getLayoutScale( width, height );
+        this.setScaleMagnitude( scale );
+
+        //center vertically
+        if ( scale === width / this.layoutBounds.width ) {
+          this.translate( 0, (height - this.layoutBounds.height * scale) / 2 / scale );
+        }
+
+        //center horizontally
+        else if ( scale === height / this.layoutBounds.height ) {
+          this.translate( (width - this.layoutBounds.width * scale) / 2 / scale, 0 );
+        }
+      }
     },
 
-    //Default layout function uses the layoutWidth and layoutHeight to scale the content (based on whichever is more limiting: width or height)
-    //and centers the content in the screen vertically and horizontally
-    //This function can be replaced by subclasses that wish to perform their own custom layout.
-    layout: function( width, height ) {
-      this.resetTransform();
-
-      var scale = this.getLayoutScale( width, height );
-      this.setScaleMagnitude( scale );
-
-      //center vertically
-      if ( scale === width / this.layoutBounds.width ) {
-        this.translate( 0, (height - this.layoutBounds.height * scale) / 2 / scale );
-      }
-
-      //center horizontally
-      else if ( scale === height / this.layoutBounds.height ) {
-        this.translate( (width - this.layoutBounds.width * scale) / 2 / scale, 0 );
-      }
+    //statics
+    {
+      LAYOUT_BOUNDS: LAYOUT_BOUNDS
     }
-  } );
+  );
 
   return ScreenView;
 } );
