84bb2bee64b1a3eaa92833a8fe17d10b16541f49
samreid <reids@colorado.edu>
2013-11-12 08:47:20 -0700

    minor style changes

--- js/HomeButton.js
+++ js/HomeButton.js
@@ -1,6 +1,5 @@
 // Copyright 2002-2013, University of Colorado Boulder
 
-//TODO this should extend sun.Button
 //TODO this should handle wiring up the callback that goes to the home screen, currently done in NavigationBar
 /**
  * The Home button that appears in the navigation bar.
@@ -8,7 +7,6 @@
 define( function( require ) {
   'use strict';
 
-  // imports
   var FontAwesomeNode = require( 'SUN/FontAwesomeNode' );
   var inherit = require( 'PHET_CORE/inherit' );
   var Node = require( 'SCENERY/nodes/Node' );
@@ -22,7 +20,10 @@ define( function( require ) {
     var icon = function( fill, highlighted ) {
       var node = new FontAwesomeNode( 'home', { fill: fill, scale: 0.75 } );
       if ( highlighted ) {
-        return new Node( {children: [node, new HighlightNode( node.width + 4, node.height, {center: node.center, whiteHighlight: !whiteColorScheme} )]} );
+        return new Node( {children: [
+          node,
+          new HighlightNode( node.width + 4, node.height, {center: node.center, whiteHighlight: !whiteColorScheme} )
+        ]} );
       }
       else {
         return node;
