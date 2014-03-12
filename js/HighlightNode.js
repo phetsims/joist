739e7ef46f99af91259cbae59a36983de87df836
Jonathan Olson <olsonsjc@gmail.com>
2014-03-12 16:27:36 -0600

    Converting zero-area-bounding-box line segments to rectangles, to prevent SVG issues. See https://github.com/phetsims/scenery/issues/196

--- js/HighlightNode.js
+++ js/HighlightNode.js
@@ -7,8 +7,7 @@ define( function( require ) {
   'use strict';
 
   var Node = require( 'SCENERY/nodes/Node' );
-  var Path = require( 'SCENERY/nodes/Path' );
-  var Shape = require( 'KITE/Shape' );
+  var Rectangle = require( 'SCENERY/nodes/Rectangle' );
   var LinearGradient = require( 'SCENERY/util/LinearGradient' );
   var inherit = require( 'PHET_CORE/inherit' );
 
@@ -19,13 +18,11 @@ define( function( require ) {
 
     var outerColor = options.whiteHighlight ? 'black' : 'white';
     var innerColor = options.whiteHighlight ? 'white' : 'black';
-    var leftBar = new Path( Shape.lineSegment( 0, 0, 0, height ), {
-      lineWidth: 1,
-      stroke: new LinearGradient( 0, 0, 0, height ).addColorStop( 0, outerColor ).addColorStop( 0.5, innerColor ).addColorStop( 1, outerColor )
+    var leftBar = new Rectangle( -0.5, 0, 1, height, 0, 0, {
+      fill: new LinearGradient( 0, 0, 0, height ).addColorStop( 0, outerColor ).addColorStop( 0.5, innerColor ).addColorStop( 1, outerColor )
     } );
-    var rightBar = new Path( Shape.lineSegment( width, 0, width, height ), {
-      lineWidth: 1,
-      stroke: new LinearGradient( 0, 0, 0, height ).addColorStop( 0, outerColor ).addColorStop( 0.5, innerColor ).addColorStop( 1, outerColor )
+    var rightBar = new Rectangle( width - 0.5, 0, 1, height, 0, 0, {
+      fill: new LinearGradient( 0, 0, 0, height ).addColorStop( 0, outerColor ).addColorStop( 0.5, innerColor ).addColorStop( 1, outerColor )
     } );
 
     options = _.extend( { children: [leftBar, rightBar], pickable: false}, options );
