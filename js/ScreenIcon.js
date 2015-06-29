123108a2bc04e2fcc0e3bdf4b0c8523c46f6ac06
Chris Malley <cmalley@pixelzoom.com>
2015-06-29 10:20:19 -0600

    #259 created ScreenIcon

---
+++ js/ScreenIcon.js
@@ -0,0 +1,52 @@
+// Copyright 2002-2015, University of Colorado Boulder
+
+/**
+ * An icon suitable for one of the screen-selection buttons on the home screen or navigation bar.
+ * By default, the size is optimized for the home screen.
+ * To optimize for the navigation bar, use options.size: Screen.NAVBAR_ICON_SIZE
+ *
+ * @author Chris Malley (PixelZoom, Inc.)
+ */
+define( function( require ) {
+  'use strict';
+
+  // modules
+  var inherit = require( 'PHET_CORE/inherit' );
+  var Node = require( 'SCENERY/nodes/Node' );
+  var Rectangle = require( 'SCENERY/nodes/Rectangle' );
+  var Screen = require( 'JOIST/Screen' );
+
+  /**
+   * @param {Node} iconNode
+   * @param options
+   * @returns {*}
+   * @constructor
+   */
+  function ScreenIcon( iconNode, options ) {
+
+    options = _.extend( {
+      size: Screen.HOME_SCREEN_ICON_SIZE, // {Dimension2} size of the background
+      xScaleFactor: 0.85, // max percentage of the background width occupied by iconNode, [0,1]
+      yScaleFactor: 0.85, // max percentage of the background height occupied by iconNode, [0,1]
+      fill: 'white', // {Color|string} background fill
+      stroke: null // {Color|string} background stroke
+    }, options );
+
+    assert && assert( options.xScaleFactor > 0 && options.xScaleFactor <= 1 );
+    assert && assert( options.yScaleFactor > 0 && options.yScaleFactor <= 1 );
+
+    var background = new Rectangle( 0, 0, options.size.width, options.size.height, {
+      fill: options.fill,
+      stroke: options.stroke
+    } );
+
+    iconNode.setScaleMagnitude( Math.min( options.xScaleFactor * background.width / iconNode.width, options.yScaleFactor * background.height / iconNode.height ) );
+    iconNode.center = background.center;
+    iconNode.pickable = false;
+
+    options.children = [ background, iconNode ];
+    Node.call( this, options );
+  }
+
+  return inherit( Node, ScreenIcon );
+} );
