46cd94050a65b3e4a555ef34ca9ccc07f4fff6b0
Chris Malley <cmalley@pixelzoom.com>
2015-07-06 10:19:34 -0600

    #268 fix comment

--- js/NavigationBar.js
+++ js/NavigationBar.js
@@ -138,8 +138,7 @@ define( function( require ) {
         // Center the screen buttons
         this.screenButtonsParent.center = this.background.center;
 
-        //TODO #268 This comment makes no sense, the home button is actually smaller than the screen buttons.
-        // Center the home icon vertically and make it a bit larger than screen buttons, see https://github.com/phetsims/joist/issues/127
+        // Center the home icon vertically and make it a bit larger than icons on screen buttons, see https://github.com/phetsims/joist/issues/127
         this.homeButton.setScaleMagnitude( 0.825 * scale );
         this.homeButton.left = this.screenButtonsParent.right + 15;
         this.homeButton.centerY = this.background.centerY;
