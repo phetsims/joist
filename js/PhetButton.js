558035b3314ade98172c381be51be71dbb7bdeaa
Chris Malley <cmalley@pixelzoom.com>
2015-07-06 09:22:55 -0600

    #271 add TODO items at usage sites

--- js/PhetButton.js
+++ js/PhetButton.js
@@ -90,6 +90,7 @@ define( function( require ) {
 
     JoistButton.call( this, icon, backgroundFillProperty, options );
 
+    //TODO #271 Why is there a specific flag for identifying the "adapted-from-phet" brand, but a string test for identifying the "phet" brand?
     // If this is an "adapted from PhET" brand, decorate the PhET button with "adapted from" text.
     if ( Brand.adaptedFromPhET ) {
       this.addChild( new AdaptedFromText( textFillProperty, {
