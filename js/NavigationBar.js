1975569d90e92e0db6730a5d57a0e659c9d29ce3
Chris Malley <cmalley@pixelzoom.com>
2016-02-05 14:58:27 -0700

    remove odd line break in comment

--- js/NavigationBar.js
+++ js/NavigationBar.js
@@ -47,8 +47,7 @@ define( function( require ) {
   var PHET_BUTTON_BOTTOM_MARGIN = PhetButton.VERTICAL_INSET; // same position as PhetButton on home screen
   var HOME_BUTTON_LEFT_MARGIN = 8;
   var SCREEN_BUTTON_SPACING = 0;
-  var MINIMUM_SCREEN_BUTTON_WIDTH = 60; // Make sure each button is at least a minimum width so they don't get too close
-                                        // together, see #279
+  var MINIMUM_SCREEN_BUTTON_WIDTH = 60; // Make sure each button is at least a minimum width so they don't get too close together, see #279
 
   /**
    * Creates a nav bar.
