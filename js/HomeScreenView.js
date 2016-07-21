029ba11e67acb68df1f7d65a3ec40eacef4bea56
samreid <reids@colorado.edu>
2016-07-21 15:21:02 -0600

    check for type existence before calling addinstance

--- js/HomeScreenView.js
+++ js/HomeScreenView.js
@@ -129,7 +129,7 @@ define( function( require ) {
       // Even though in the user interface, the small and large buttons seem like a single UI component
       // that has grown larger, it would be quite a headache to create a composite button for the purposes of
       // tandem, so instead the large and small buttons are registered as separate instances.  See https://github.com/phetsims/phet-io/issues/99
-      options.tandem && options.tandem.createTandem( screen.tandem.tail + 'LargeButton' ).addInstance( largeScreenButton, TScreenButton );
+      TScreenButton && options.tandem && options.tandem.createTandem( screen.tandem.tail + 'LargeButton' ).addInstance( largeScreenButton, TScreenButton );
 
       // TODO: Switch to buttonListener, but make sure you test it because on 7/17/2013 there is a problem where
       // TODO: ButtonListener won't fire if a node has appeared under the pointer
@@ -212,7 +212,7 @@ define( function( require ) {
         }
       } );
 
-      options.tandem && options.tandem.createTandem( screen.tandem.tail + 'SmallButton' ).addInstance( smallScreenButton, TScreenButton );
+      TScreenButton && options.tandem && options.tandem.createTandem( screen.tandem.tail + 'SmallButton' ).addInstance( smallScreenButton, TScreenButton );
 
       var highlightListener = {
         over: function( event ) {
