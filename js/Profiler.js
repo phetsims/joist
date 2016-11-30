0a094e4b072be26eb2d7e596163c18f71039a601
Chris Malley <cmalley@pixelzoom.com>
2016-11-29 18:59:48 -0700

    Profiler: improve documentation and display formatting, https://github.com/phetsims/joist/issues/168

--- js/Profiler.js
+++ js/Profiler.js
@@ -1,15 +1,32 @@
-// Copyright 2014-2015, University of Colorado Boulder
+// Copyright 2014-2016, University of Colorado Boulder
 
 /**
  * This minimalistic profiler is meant to help understand the time spent in running a PhET simulation.
  * It was designed to be minimally invasive, so it won't alter the simulation's performance significantly.
  * Note: just showing the average FPS or ms/frame is not sufficient, since we need to see when garbage collections
- * happen, which are typically a spike in a single frame.  Hence, the data is shown as a histogram.  After the first
- * 30ms slots, there is a ++= showing the times of longer frames (in ms)
+ * happen, which are typically a spike in a single frame.  Hence, the data is shown as a histogram. Data that
+ * doesn't fit in the histogram appears in an optional 'longTimes' field.
+ *
+ * Output is displayed in the upper-left corner of the browser window.
+ *
+ * The general format is:
+ *
+ * FPS - ms/frame - histogram [- longTimes]
+ *
+ * Here's an example:
+ *
+ * 48 FPS - 21ms/frame - 0,0,5,0,0,0,0,0,1,0,0,0,0,3,1,3,18,19,5,3,1,0,1,0,0,0,0,1,0,0 - 50,37,217
+ *
+ * The histogram field is a sequence of 30 numbers, for 0-29ms. Each number indicates the number of frames that took
+ * that amount of time. In the above example, there were 5 frames that took 2ms.
+ *
+ * The longTimes field is the set of frame times that exceeded 29ms, and thus don't fit in the histogram.
+ * If 2 frames took 37ms, then 37ms will appear twice.  If no frames exceeded 29ms, then this field will be absent.
  *
  * TODO: Could take further steps to helping identify the time spent in render vs input handling vs model, etc.
  *
  * @author Sam Reid (PhET Interactive Simulations)
+ * @author Chris Malley (PixelZoom, Inc.)
  */
 define( function( require ) {
   'use strict';
@@ -18,15 +35,17 @@ define( function( require ) {
   var inherit = require( 'PHET_CORE/inherit' );
   var joist = require( 'JOIST/joist' );
 
+  // constants
+  var FIELD_SEPARATOR = ' \u2014 '; // em dash, a long horizontal dash
+
   /**
    * Construct a Profiler
    * @constructor
    */
   function Profiler() {
 
+    //TODO Now that profiler is enabled via a query parameter, we should use more sensible/flexible data structures here.
     // These data structured were chosen to minimize CPU time.
-    // TODO: Now that profiler is buried under a query parameter, we could use more sensible/flexible data structures
-    // here.
     this.histogram = []; // @private
     this.longTimes = []; // @private
     this.allTimes = [];  // @private
@@ -52,19 +71,31 @@ define( function( require ) {
 
       var timeBetweenFrames = this.frameStartTime - this.lastFrameStartTime;
       if ( this.frameCount % 60 === 0 ) {
+
         var totalTime = 0;
         for ( var i = 0; i < this.allTimes.length; i++ ) {
           totalTime += this.allTimes[ i ];
         }
-        var averageFrameTime = Math.round( totalTime / this.allTimes.length );
-        var averageFPS = Math.round( 1000 / (totalTime / this.allTimes.length) );
 
-        var text = '' + averageFPS + ' FPS, ' + averageFrameTime + 'ms/frame, ';
-        text = text + this.histogram;
+        // FPS
+        var averageFPS = Math.round( 1000 / (totalTime / this.allTimes.length) );
+        var text = '' + averageFPS + ' FPS';
+
+        // ms/frame
+        var averageFrameTime = Math.round( totalTime / this.allTimes.length );
+        text = text + FIELD_SEPARATOR + averageFrameTime + 'ms/frame';
+
+        // histogram is for frame rates from 0-29ms, where the value is the number of frames for each frame rate.
+        text = text + FIELD_SEPARATOR + this.histogram;
+
+        // longTimes includes any frame rates that exceed 29ms, and therefore don't fit in the histogram.
         if ( this.longTimes.length ) {
-          text = text + ', +++ = ' + this.longTimes;
+          text = text + FIELD_SEPARATOR + this.longTimes;
         }
+
         $( '#trace' ).html( text );
+
+        // clear data structures
         for ( i = 0; i < 30; i++ ) {
           this.histogram[ i ] = 0;
         }
