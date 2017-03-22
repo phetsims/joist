e9e4056af9b0515916ccb507a4e5129b70285bbe
Jesse Greenberg <jesse.greenberg@colorado.edu>
2017-03-22 16:24:49 -0600

    Remove AccessiblePeer from LinkText

--- js/LinkText.js
+++ js/LinkText.js
@@ -12,30 +12,13 @@ define( function( require ) {
   var inherit = require( 'PHET_CORE/inherit' );
   var Text = require( 'SCENERY/nodes/Text' );
   var ButtonListener = require( 'SCENERY/input/ButtonListener' );
-  var AccessiblePeer = require( 'SCENERY/accessibility/AccessiblePeer' );
   var joist = require( 'JOIST/joist' );
 
   function LinkText( text, url, options ) {
 
     // defaults
     options = _.extend( {
-      handleEvent: false, // whether the up in the click event should be handled, e.g. to prevent the About dialog closing.
-      accessibleContent: {
-        createPeer: function( accessibleInstance ) {
-
-          /* We want the accessible peer to look like the following in the Parallel DOM.
-           *
-           * <a href=url tabindex='0' target="_blank">text></a>
-           */
-          var domElement = document.createElement( 'a' );
-          domElement.tabIndex = '0'; // in tab order by default
-          domElement.href = url; // define link address
-          domElement.innerText = text; // inner description, read by screen readers
-          domElement.setAttribute( 'target', '_blank' ); // open in a new window/tab
-
-          return new AccessiblePeer( accessibleInstance, domElement );
-        }
-      }
+      handleEvent: false // whether the up in the click event should be handled, e.g. to prevent the About dialog closing.
     }, options );
 
     Text.call( this, text, _.extend( {
