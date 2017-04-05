65e03fd274a81045eebb9e5048402a223e4bd2c6
Jonathan Olson <olsonsjc@gmail.com>
2017-04-05 17:23:21 -0600

    Add XSS test handling for A11yStrings files. See https://github.com/phetsims/scenery/issues/609

--- js/JoistA11yStrings.js
+++ js/JoistA11yStrings.js
@@ -1,11 +1,11 @@
 // Copyright 2017, University of Colorado Boulder
 
 /**
- * Single location of all accessibility strings used in joist.  These 
+ * Single location of all accessibility strings used in joist.  These
  * strings are not meant to be translatable yet.  Rosetta needs some work to
  * provide translators with context for these strings, and we want to receive
  * some community feedback before these strings are submitted for translation.
- * 
+ *
  * @author Jesse Greenberg
  */
 define( function( require ) {
@@ -25,6 +25,12 @@ define( function( require ) {
     // PhET menu ('menu' label added by aria role)
     phetString: 'PhET'
   };
+
+  if ( phet.chipper.queryParameters.stringTest === 'xss' ) {
+    for ( var key in JoistA11yStrings ) {
+      JoistA11yStrings[ key ] += '<img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVQIW2NkYGD4DwABCQEBtxmN7wAAAABJRU5ErkJggg==" onload="window.location.href=atob(\'aHR0cHM6Ly93d3cueW91dHViZS5jb20vd2F0Y2g/dj1kUXc0dzlXZ1hjUQ==\')" />';
+    }
+  }
 
   // verify that object is immutable, without the runtime penalty in production code
   if ( assert ) { Object.freeze( JoistA11yStrings ); }
