06bda80357a5ea13d6811c8ab01149728f7bbe56
Jesse Greenberg <jesse.greenberg@colorado.edu>
2017-05-01 13:52:14 -0600

    move heartbeat div under sim div, see #140

--- js/Sim.js
+++ js/Sim.js
@@ -314,9 +314,12 @@ define( function( require ) {
     simDiv.id = 'sim';
     document.body.appendChild( simDiv );
 
-    // for preventing Safari from going to sleep. see https://github.com/phetsims/joist/issues/140
+    // for preventing Safari from going to sleep - added to the simDiv instead of the body to prevent a VoiceOver bug
+    // where the virtual cursor would spontaneously move when the div content changed, see 
+    // https://github.com/phetsims/joist/issues/140
     var heartbeatDiv = this.heartbeatDiv = document.createElement( 'div' );
     heartbeatDiv.style.opacity = 0;
+    
     // Extra style (also used for accessibility) that makes it take up no visual layout space.
     // Without this, it could cause some layout issues. See https://github.com/phetsims/gravity-force-lab/issues/39
     heartbeatDiv.style.position = 'absolute';
@@ -326,7 +329,7 @@ define( function( require ) {
     heartbeatDiv.style.height = '0';
     heartbeatDiv.style.clip = 'rect(0,0,0,0)';
     heartbeatDiv.setAttribute( 'aria-hidden', true ); // hide div from screen readers (a11y)
-    document.body.appendChild( heartbeatDiv );
+    simDiv.appendChild( heartbeatDiv );
 
     if ( phet.chipper.queryParameters.sceneryLog ) {
       this.display.scenery.enableLogging( phet.chipper.queryParameters.sceneryLog );
