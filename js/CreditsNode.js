e3c3c82fbc5102524cddef4adb5a007ffc285a64
zepumph <michael.kauzmann@colorado.edu>
2017-08-10 14:08:55 -0800

    MultiLineText -> RichText in CreditsNode, https://github.com/phetsims/joist/issues/435 https://github.com/phetsims/joist/issues/71

--- js/CreditsNode.js
+++ js/CreditsNode.js
@@ -11,36 +11,37 @@ define( function( require ) {
   'use strict';
 
   // modules
-  var VBox = require( 'SCENERY/nodes/VBox' );
-  var Text = require( 'SCENERY/nodes/Text' );
-  var inherit = require( 'PHET_CORE/inherit' );
-  var PhetFont = require( 'SCENERY_PHET/PhetFont' );
-  var MultiLineText = require( 'SCENERY_PHET/MultiLineText' );
-  var StringUtils = require( 'PHETCOMMON/util/StringUtils' );
-  var VStrut = require( 'SCENERY/nodes/VStrut' );
   var joist = require( 'JOIST/joist' );
+  var inherit = require( 'PHET_CORE/inherit' );
+  var StringUtils = require( 'PHETCOMMON/util/StringUtils' );
+  var RichText = require( 'SCENERY/nodes/RichText' );
+  var Text = require( 'SCENERY/nodes/Text' );
+  var VBox = require( 'SCENERY/nodes/VBox' );
+  var VStrut = require( 'SCENERY/nodes/VStrut' );
+  // var MultiLineText = require( 'SCENERY_PHET/MultiLineText' );
+  var PhetFont = require( 'SCENERY_PHET/PhetFont' );
 
   // strings
-  var creditsTitleString = require( 'string!JOIST/credits.title' );
+  var creditsContributorsString = require( 'string!JOIST/credits.contributors' );
+  var creditsGraphicArtsString = require( 'string!JOIST/credits.graphicArts' );
   var creditsLeadDesignString = require( 'string!JOIST/credits.leadDesign' );
+  var creditsQualityAssuranceString = require( 'string!JOIST/credits.qualityAssurance' );
   var creditsSoftwareDevelopmentString = require( 'string!JOIST/credits.softwareDevelopment' );
   var creditsTeamString = require( 'string!JOIST/credits.team' );
-  var creditsContributorsString = require( 'string!JOIST/credits.contributors' );
-  var creditsQualityAssuranceString = require( 'string!JOIST/credits.qualityAssurance' );
-  var creditsGraphicArtsString = require( 'string!JOIST/credits.graphicArts' );
-  var creditsTranslationString = require( 'string!JOIST/credits.translation' );
   var creditsThanksString = require( 'string!JOIST/credits.thanks' );
+  var creditsTitleString = require( 'string!JOIST/credits.title' );
+  var creditsTranslationString = require( 'string!JOIST/credits.translation' );
 
   /**
    * Creates node that displays the credits.
    * @param {Object} credits - see implementation herein for supported {string} fields
-   * @param {Object} [options] - Passed to VBox
+   * @param {Object} [options] - Passed to VBox and RichText
    * @constructor
    */
   function CreditsNode( credits, options ) {
     var titleFont = new PhetFont( { size: 14, weight: 'bold' } );
     var font = new PhetFont( 12 );
-    var multiLineTextOptions = { font: font, align: 'left' };
+    var multiLineTextOptions = { font: font, align: 'left', lineWrap: options.maxWidth };
     var children = [];
 
     // Credits
@@ -52,22 +53,22 @@ define( function( require ) {
       accessibleLabel: creditsTitleString
     } ) );
     if ( credits.leadDesign ) {
-      children.push( new MultiLineText( StringUtils.format( creditsLeadDesignString, '\u202a' + credits.leadDesign + '\u202c' ), multiLineTextOptions ) );
+      children.push( new RichText( StringUtils.format( creditsLeadDesignString, '\u202a' + credits.leadDesign + '\u202c' ), multiLineTextOptions ) );
     }
     if ( credits.softwareDevelopment ) {
-      children.push( new MultiLineText( StringUtils.format( creditsSoftwareDevelopmentString, '\u202a' + credits.softwareDevelopment + '\u202c' ), multiLineTextOptions ) );
+      children.push( new RichText( StringUtils.format( creditsSoftwareDevelopmentString, '\u202a' + credits.softwareDevelopment + '\u202c' ), multiLineTextOptions ) );
     }
     if ( credits.team ) {
-      children.push( new MultiLineText( StringUtils.format( creditsTeamString, '\u202a' + credits.team + '\u202c' ), multiLineTextOptions ) );
+      children.push( new RichText( StringUtils.format( creditsTeamString, '\u202a' + credits.team + '\u202c' ), multiLineTextOptions ) );
     }
     if ( credits.contributors ) {
-      children.push( new MultiLineText( StringUtils.format( creditsContributorsString, '\u202a' + credits.contributors + '\u202c' ), multiLineTextOptions ) );
+      children.push( new RichText( StringUtils.format( creditsContributorsString, '\u202a' + credits.contributors + '\u202c' ), multiLineTextOptions ) );
     }
     if ( credits.qualityAssurance ) {
-      children.push( new MultiLineText( StringUtils.format( creditsQualityAssuranceString, '\u202a' + credits.qualityAssurance + '\u202c' ), multiLineTextOptions ) );
+      children.push( new RichText( StringUtils.format( creditsQualityAssuranceString, '\u202a' + credits.qualityAssurance + '\u202c' ), multiLineTextOptions ) );
     }
     if ( credits.graphicArts ) {
-      children.push( new MultiLineText( StringUtils.format( creditsGraphicArtsString, '\u202a' + credits.graphicArts + '\u202c' ), multiLineTextOptions ) );
+      children.push( new RichText( StringUtils.format( creditsGraphicArtsString, '\u202a' + credits.graphicArts + '\u202c' ), multiLineTextOptions ) );
     }
 
     //TODO obtain translation credit from strings file, see https://github.com/phetsims/joist/issues/163
@@ -79,7 +80,7 @@ define( function( require ) {
         tagName: 'h2',
         accessibleLabel: creditsTranslationString
       } ) );
-      children.push( new MultiLineText( credits.translation, multiLineTextOptions ) );
+      children.push( new RichText( credits.translation, multiLineTextOptions ) );
     }
 
     // Thanks
@@ -90,7 +91,7 @@ define( function( require ) {
         tagName: 'h2',
         accessibleLabel: creditsThanksString
       } ) );
-      children.push( new MultiLineText( credits.thanks, multiLineTextOptions ) );
+      children.push( new RichText( credits.thanks, multiLineTextOptions ) );
     }
 
     VBox.call( this, _.extend( {
