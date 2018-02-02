f8641b81d92e2d0f71c59c70116f7125febb7040
Jesse Greenberg <jesse.greenberg@colorado.edu>
2018-02-02 16:02:15 -0700

    instrument close button path in KeyboardHelpDialog, see #445

--- js/KeyboardHelpDialog.js
+++ js/KeyboardHelpDialog.js
@@ -77,6 +77,7 @@ define( function( require ) {
       lineCap: 'round',
       lineWidth: 2,
       cursor: 'pointer',
+      tandem: options.tandem.createTandem( 'closeButtonPath' ),
 
       // a11y
       tagName: 'button',
