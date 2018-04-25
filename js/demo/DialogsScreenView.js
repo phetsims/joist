866f20560d224e65e582f3f021458e9e17f608e2
Chris Malley <cmalley@pixelzoom.com>
2018-04-25 12:43:22 -0600

    KeyboardHelpButton, replace simLookAndFeel with navigationBarFillProperty, #486, #487
    Signed-off-by: Chris Malley <cmalley@pixelzoom.com>

--- js/demo/DialogsScreenView.js
+++ js/demo/DialogsScreenView.js
@@ -13,12 +13,9 @@ define( function( require ) {
   var inherit = require( 'PHET_CORE/inherit' );
   var joist = require( 'JOIST/joist' );
   var KeyboardHelpButton = require( 'JOIST/KeyboardHelpButton' );
-  var KeyboardHelpDialog = require( 'JOIST/KeyboardHelpDialog' );
-  var PhetFont = require( 'SCENERY_PHET/PhetFont' );
-  var RectangularPushButton = require( 'SUN/buttons/RectangularPushButton' );
+  var Property = require( 'AXON/Property' );
   var ScreenView = require( 'JOIST/ScreenView' );
   var Tandem = require( 'TANDEM/Tandem' );
-  var Text = require( 'SCENERY/nodes/Text' );
   var VBox = require( 'SCENERY/nodes/VBox' );
 
   /**
@@ -30,21 +27,14 @@ define( function( require ) {
 
     var keyboardHelpDialogContent = new GeneralNavigationHelpContent();
 
-    //TODO #486 why doesn't this render?
-    var keyboardHelpButton = new KeyboardHelpButton( keyboardHelpDialogContent, phet.joist.sim.lookAndFeel, Tandem.optional );
+    var backgroundColorProperty = new Property( 'white' );
 
-    //TODO #486 workaround, delete this when keyboardHelpButton renders
-    var myButton = new RectangularPushButton( {
-      content: new Text( 'keyboard help', { font: new PhetFont( 20 ) } ),
-      listener: function() {
-        new KeyboardHelpDialog( myButton, keyboardHelpDialogContent ).show();
-      }
-    } );
+    var keyboardHelpButton = new KeyboardHelpButton( keyboardHelpDialogContent, backgroundColorProperty, Tandem.optional );
+    keyboardHelpButton.setScaleMagnitude( 2 );
 
     this.addChild( new VBox( {
       children: [
-        keyboardHelpButton,
-        myButton
+        keyboardHelpButton
       ],
       spacing: 20,
       center: this.layoutBounds.center
