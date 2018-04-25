375d3e39cd0d69e8e17b86ccc8752f631c3f1364
Chris Malley <cmalley@pixelzoom.com>
2018-04-25 12:13:24 -0600

    add keyboard help to demo #486
    Signed-off-by: Chris Malley <cmalley@pixelzoom.com>

--- js/demo/DialogsScreenView.js
+++ js/demo/DialogsScreenView.js
@@ -9,11 +9,17 @@ define( function( require ) {
   'use strict';
 
   // modules
+  var GeneralNavigationHelpContent = require( 'SCENERY_PHET/keyboard/help/GeneralNavigationHelpContent' );
   var inherit = require( 'PHET_CORE/inherit' );
   var joist = require( 'JOIST/joist' );
+  var KeyboardHelpButton = require( 'JOIST/KeyboardHelpButton' );
+  var KeyboardHelpDialog = require( 'JOIST/KeyboardHelpDialog' );
   var PhetFont = require( 'SCENERY_PHET/PhetFont' );
+  var RectangularPushButton = require( 'SUN/buttons/RectangularPushButton' );
   var ScreenView = require( 'JOIST/ScreenView' );
+  var Tandem = require( 'TANDEM/Tandem' );
   var Text = require( 'SCENERY/nodes/Text' );
+  var VBox = require( 'SCENERY/nodes/VBox' );
 
   /**
    * @constructor
@@ -22,8 +28,25 @@ define( function( require ) {
 
     ScreenView.call( this );
 
-    this.addChild( new Text( 'TODO: add tests for Dialog subtypes', {
-      font: new PhetFont( 24 ),
+    var keyboardHelpDialogContent = new GeneralNavigationHelpContent();
+
+    //TODO #486 why doesn't this render?
+    var keyboardHelpButton = new KeyboardHelpButton( keyboardHelpDialogContent, phet.joist.sim.lookAndFeel, Tandem.optional );
+
+    //TODO #486 workaround, delete this when keyboardHelpButton renders
+    var myButton = new RectangularPushButton( {
+      content: new Text( 'keyboard help', { font: new PhetFont( 20 ) } ),
+      listener: function() {
+        new KeyboardHelpDialog( myButton, keyboardHelpDialogContent ).show();
+      }
+    } );
+
+    this.addChild( new VBox( {
+      children: [
+        keyboardHelpButton,
+        myButton
+      ],
+      spacing: 20,
       center: this.layoutBounds.center
     } ) );
   }
