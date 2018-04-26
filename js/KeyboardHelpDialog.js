cc29c5d95ff2eeb6ac75944fa011fcaf0c0bb582
zepumph <michael.kauzmann@colorado.edu>
2018-04-26 13:16:51 -0800

    remove custom close button, https://github.com/phetsims/joist/issues/486

--- js/KeyboardHelpDialog.js
+++ js/KeyboardHelpDialog.js
@@ -10,22 +10,15 @@ define( function( require ) {
   'use strict';
 
   // modules
-  var ButtonListener = require( 'SCENERY/input/ButtonListener' );
   var Dialog = require( 'SUN/Dialog' );
-  var FocusHighlightPath = require( 'SCENERY/accessibility/FocusHighlightPath' );
   var inherit = require( 'PHET_CORE/inherit' );
   var joist = require( 'JOIST/joist' );
   var JoistA11yStrings = require( 'JOIST/JoistA11yStrings' );
-  var Path = require( 'SCENERY/nodes/Path' );
   var PhetFont = require( 'SCENERY_PHET/PhetFont' );
-  var Shape = require( 'KITE/Shape' );
-  var SunA11yStrings = require( 'SUN/SunA11yStrings' );
   var Tandem = require( 'TANDEM/Tandem' );
   var Text = require( 'SCENERY/nodes/Text' );
 
   // constants
-  var CLOSE_BUTTON_WIDTH = 7;
-  var CLOSE_BUTTON_MARGIN = 10;
   var HELP_CONTENT_MARGIN = 20;
   var TITLE_MAX_WIDTH = 500;
 
@@ -33,7 +26,6 @@ define( function( require ) {
   var keyboardShortcutsTitleString = require( 'string!JOIST/keyboardShortcuts.title' );
 
   // a11y string
-  var closeString = SunA11yStrings.close.value;
   var hotKeysAndHelpString = JoistA11yStrings.hotKeysAndHelp.value;
 
   /**
@@ -69,53 +61,13 @@ define( function( require ) {
     // help content surrounded by a div unless already specified, so that all content is read when dialog opens
     helpContent.tagName = helpContent.tagName || 'div';
 
-    // shape and path for a custom close button
-    var closeButtonShape = new Shape();
-    closeButtonShape.moveTo( -CLOSE_BUTTON_WIDTH, -CLOSE_BUTTON_WIDTH ).lineTo( CLOSE_BUTTON_WIDTH, CLOSE_BUTTON_WIDTH );
-    closeButtonShape.moveTo( CLOSE_BUTTON_WIDTH, -CLOSE_BUTTON_WIDTH ).lineTo( -CLOSE_BUTTON_WIDTH, CLOSE_BUTTON_WIDTH );
-
-    // @public (joist-internal)
-    this.closeButtonPath = new Path( closeButtonShape, {
-      stroke: 'red', //TODO #486 to identify the custom close button that needs to be removed
-      lineCap: 'round',
-      lineWidth: 2,
-      cursor: 'pointer',
-      tandem: options.tandem.createTandem( 'closeButtonPath' ),
-
-      // a11y
-      tagName: 'button',
-      innerContent: closeString
-    } );
-    var dilationCoefficient = FocusHighlightPath.getDilationCoefficient( this.closeButtonPath );
-    this.closeButtonPath.focusHighlight = Shape.bounds( closeButtonShape.getBounds().dilated( dilationCoefficient ) );
-
     // add a listener to hide the dialog
     var self = this;
-    var buttonListener = new ButtonListener( {
-      down: function() {
-        self.hide();
-      }
-    } );
-    this.closeButtonPath.addInputListener( buttonListener );
-
-    // mouse/touch areas for the close button
-    var areaX = this.closeButtonPath.left - this.closeButtonPath.width * 2;
-    var areaY = this.closeButtonPath.top - CLOSE_BUTTON_MARGIN / 2;
-    var width = this.closeButtonPath.width * 4;
-    var height = this.closeButtonPath.height + CLOSE_BUTTON_MARGIN;
-    this.closeButtonPath.mouseArea = Shape.rect( areaX, areaY, width, height );
-    this.closeButtonPath.touchArea = this.closeButtonPath.mouseArea;
-
     Dialog.call( this, helpContent, options );
 
-    // position and add the close button
-    this.closeButtonPath.right = helpContent.right + 2 * HELP_CONTENT_MARGIN - CLOSE_BUTTON_MARGIN;
-    this.closeButtonPath.top = helpContent.top + CLOSE_BUTTON_MARGIN;
-    this.addChild( this.closeButtonPath );
-
-    // a11y - the close button comes first so that the remaining content can easily be read with the screen reader's
-    // virtual cursor
-    this.accessibleOrder = [ this.closeButtonPath, options.title ];
+    // a11y - the close button comes first so that the remaining content
+    // can easily be read with the screen reader's virtual cursor
+    this.accessibleOrder = [ this.closeButton, options.title ];
 
     // @private (a11y) - input listener for the close button, must be disposed
     var clickListener = {
@@ -128,8 +80,7 @@ define( function( require ) {
 
     // @private - to be called by dispose
     this.disposeKeyboardHelpDialog = function() {
-      self.closeButtonPath.removeInputListener( buttonListener );
-      self.closeButtonPath.removeAccessibleInputListener( clickListener );
+      self.removeAccessibleInputListener( clickListener );
     };
   }
 
@@ -139,10 +90,21 @@ define( function( require ) {
 
     /**
      * So the Dialog is eligible for garbage collection.
+     * @public
+     * @override
      */
     dispose: function() {
       this.disposeKeyboardHelpDialog();
       Dialog.prototype.dispose.call( this );
+    },
+
+    /**
+     * Focus the close button of the Dialog.
+     * @a11y
+     * @public
+     */
+    focusCloseButton: function() {
+      this.closeButton.focus();
     }
   } );
 } );
