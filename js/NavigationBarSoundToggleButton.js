91786c93a1f4ede8aae39bea46925cea7fa8f361
Chris Klusendorf <chkl2587@colorado.edu>
2018-07-17 18:26:24 -0600

    Refactored NavigationBarSoundToggleButton to be drawn with scenery instead of using font awesome icons, see https://github.com/phetsims/tambo/issues/19 and https://github.com/phetsims/joist/issues/497

--- js/NavigationBarSoundToggleButton.js
+++ js/NavigationBarSoundToggleButton.js
@@ -5,28 +5,48 @@
  * navigation bar
  *
  * @author John Blanco
+ * @author Chris Klusendorf
  */
 
 define( function( require ) {
   'use strict';
 
   // modules
-  var FontAwesomeNode = require( 'SUN/FontAwesomeNode' );
   var inherit = require( 'PHET_CORE/inherit' );
   var joist = require( 'JOIST/joist' );
   var JoistA11yStrings = require( 'JOIST/JoistA11yStrings' );
   var JoistButton = require( 'JOIST/JoistButton' );
   var Node = require( 'SCENERY/nodes/Node' );
   var Path = require( 'SCENERY/nodes/Path' );
+  var Rectangle = require( 'SCENERY/nodes/Rectangle' );
   var Shape = require( 'KITE/Shape' );
   var ToggleNode = require( 'SUN/ToggleNode' );
+  var Vector2 = require( 'DOT/Vector2' );
 
   // a11y strings
   var soundOnOffString = JoistA11yStrings.soundOnOffButton.value;
 
-  // constants
-  var NODE_HEIGHT = 20; // empirically determined
-  var X_WIDTH = NODE_HEIGHT * 0.7; // empirically determined
+  // constants for node background
+  var NODE_HEIGHT = 22.0;
+  var NODE_WIDTH = NODE_HEIGHT * 1.13;
+
+  // constants for drawing speaker
+  var SPEAKER_HEIGHT = NODE_HEIGHT * 0.77;
+  var SPEAKER_WIDTH = SPEAKER_HEIGHT * 0.58;
+  var SPEAKER_BACK_WIDTH = SPEAKER_WIDTH * 0.46;
+  var SPEAKER_BACK_HEIGHT = SPEAKER_HEIGHT * 0.35;
+  var SPEAKER_BACK_Y_SPACE = ( SPEAKER_HEIGHT - SPEAKER_BACK_HEIGHT ) / 2.0; // space between top of speaker back and top of cone
+
+  // constant for drawing sound off X
+  var X_WIDTH = SPEAKER_HEIGHT * 0.52;
+
+  // constants for drawing sound on curves
+  var MAX_CURVE_RADIUS = SPEAKER_HEIGHT * 0.63;
+  var RADIUS_STEPPER = SPEAKER_HEIGHT * 0.23;
+  var MED_CURVE_RADIUS = MAX_CURVE_RADIUS - RADIUS_STEPPER;
+  var MIN_CURVE_RADIUS = MED_CURVE_RADIUS - RADIUS_STEPPER;
+  var CURVE_ANGLE = Math.PI / 2.7;
+  var NEG_CURVE_ANGLE = CURVE_ANGLE * -1.0;
 
   /**
    * @param {BooleanProperty} soundEnabledProperty
@@ -50,24 +70,66 @@ define( function( require ) {
       innerContent: soundOnOffString
     }, options );
 
-    // 'on' icon is a font-awesome icon
-    var soundOnNode = new FontAwesomeNode( 'volume_up' );
-
-    // 'off' icon is a font-awesome icon, with an 'x' added to the right.
+    var soundOnNode = new Node();
     var soundOffNode = new Node();
-    var soundOffIcon = new FontAwesomeNode( 'volume_off' );
-    soundOffNode.addChild( soundOffIcon );
+    var backgroundNode = new Rectangle( 0, 0, NODE_WIDTH, NODE_HEIGHT, { fill: 'transparent' } );
+    soundOnNode.addChild( backgroundNode );
+    soundOffNode.addChild( backgroundNode );
+
+    // create speaker for both sound on and sound off
+    var speakerNode = new Path(
+      new Shape()
+        .moveTo( SPEAKER_WIDTH, 0 )
+        .lineTo( SPEAKER_BACK_WIDTH, SPEAKER_BACK_Y_SPACE )
+        .lineTo( 0, SPEAKER_BACK_Y_SPACE )
+        .lineToRelative( 0, SPEAKER_BACK_HEIGHT )
+        .lineToRelative( SPEAKER_BACK_WIDTH, 0 )
+        .lineTo( SPEAKER_WIDTH, SPEAKER_HEIGHT )
+        .lineTo( SPEAKER_WIDTH, 0 )
+        .close(),
+      {
+        stroke: 'black',
+        lineWidth: 1.5,
+        lineJoin: 'round',
+        fill: 'black',
+        centerY: soundOffNode.centerY,
+        left: 0
+      }
+    );
+
+    soundOnNode.addChild( speakerNode );
+    soundOffNode.addChild( speakerNode );
+
+    // create X for sound off
     var soundOffX = new Path(
       new Shape().moveTo( 0, 0 ).lineTo( X_WIDTH, X_WIDTH ).moveTo( 0, X_WIDTH ).lineTo( X_WIDTH, 0 ),
       {
         stroke: 'black',
-        lineWidth: 2.5,
+        lineWidth: 1.7,
         lineCap: 'round',
-        right: soundOnNode.width, // position the 'x' so that both icons have the same width, see scenery-phet#329
+        right: soundOffNode.width,
         centerY: soundOffNode.centerY
       }
     );
     soundOffNode.addChild( soundOffX );
+
+    // create curved lines for sound on
+    var soundOnCurves = new Path(
+      new Shape()
+        .arc( 0, 0, MAX_CURVE_RADIUS, CURVE_ANGLE, NEG_CURVE_ANGLE, true )
+        .moveToPoint( new Vector2( MED_CURVE_RADIUS, 0 ).rotated( CURVE_ANGLE ) )
+        .arc( 0, 0, MED_CURVE_RADIUS, CURVE_ANGLE, NEG_CURVE_ANGLE, true )
+        .moveToPoint( new Vector2( MIN_CURVE_RADIUS, 0 ).rotated( CURVE_ANGLE ) )
+        .arc( 0, 0, MIN_CURVE_RADIUS, CURVE_ANGLE, NEG_CURVE_ANGLE, true ),
+      {
+        stroke: 'black',
+        lineWidth: 1.7,
+        lineCap: 'round',
+        right: soundOnNode.width,
+        centerY: soundOnNode.centerY
+      }
+    );
+    soundOnNode.addChild( soundOnCurves );
 
     var toggleNode = new ToggleNode(
       [
@@ -83,12 +145,10 @@ define( function( require ) {
     // change the icon so that it is visible when the navigation bar changes from dark to light
     simLookAndFeel.navigationBarDarkProperty.link( function( navigationBarDark ) {
       var baseColor = navigationBarDark ? 'black' : 'white';
-      var backgroundColor = navigationBarDark ? 'white' : 'black';
-      soundOnNode.fill = baseColor;
-      soundOnNode.stroke = backgroundColor;
-      soundOffIcon.fill = baseColor;
-      soundOffIcon.stroke = backgroundColor;
+      speakerNode.stroke = baseColor;
+      speakerNode.fill = baseColor;
       soundOffX.stroke = baseColor;
+      soundOnCurves.stroke = baseColor;
     } );
   }
 
