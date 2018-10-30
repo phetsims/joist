f5d385778c839bf687446efc41578a7691d2db7c
samreid <reids@colorado.edu>
2018-10-30 12:57:33 -0600

    Removed fullscreen menu item from mobile safari, see #524

--- js/PhetMenu.js
+++ js/PhetMenu.js
@@ -364,7 +364,7 @@ define( function( require ) {
       },
       {
         text: menuItemFullscreenString,
-        present: FullScreen.isFullScreenEnabled() && !isPhetApp,
+        present: FullScreen.isFullScreenEnabled() && !isPhetApp && !platform.mobileSafari,
         checkedProperty: FullScreen.isFullScreenProperty,
         callback: function() {
           FullScreen.toggleFullScreen( sim );
