3508822b4855924c299831d26a06bd90e28336b0
jbphet <john.blanco@colorado.edu>
2019-06-17 13:32:56 -0600

    added first cut of screen selection sound generator

---
+++ js/sound-generators/ScreenSelectionSoundGenerator.js
@@ -0,0 +1,45 @@
+// Copyright 2019, University of Colorado Boulder
+
+/**
+ * generates sounds for when the user switches between screens
+ *
+ * @author John Blanco (PhET Interactive Simulations)
+ */
+define( function( require ) {
+  'use strict';
+
+  // modules
+  const MultiClip = require( 'TAMBO/sound-generators/MultiClip' );
+  const tambo = require( 'TAMBO/tambo' );
+
+  // sounds
+  const screenSelectedSound = require( 'sound!TAMBO/screen-selection.mp3' );
+  const homeSelectedSound = require( 'sound!TAMBO/screen-selection-home-001.mp3' );
+
+  class ScreenSelectionSoundGenerator extends MultiClip {
+
+    /**
+     * @param {Property.<Screen|null>} currentScreenProperty - an axon property that indicates which sim screen is
+     * currently selected
+     */
+    constructor( currentScreenProperty ) {
+
+      super( [ homeSelectedSound, screenSelectedSound ] );
+
+      currentScreenProperty.lazyLink( currentScreen => {
+        if ( currentScreen === null ) {
+
+          // home screen sound
+          this.play( 0 );
+        }
+        else {
+
+          // non-home screen selection sound
+          this.play( 1 );
+        }
+      } );
+    }
+  }
+
+  return tambo.register( 'ScreenSelectionSoundGenerator', ScreenSelectionSoundGenerator );
+} );
