c5f2b2450b2469c714486fdcfec45e2c96de1944
Jonathan Olson <jonathan.olson@colorado.edu>
2019-08-19 11:56:33 -0600

    Guards for focusing popup windows, see https://github.com/phetsims/phet-ios-app/issues/508

--- js/PhetMenu.js
+++ js/PhetMenu.js
@@ -148,7 +148,7 @@ define( function( require ) {
           if ( !fuzzes ) {
             // Open locale-specific PhET home page. If there is no website translation for locale, fallback will be handled by server. See joist#97.
             var phetWindow = window.open( 'http://phet.colorado.edu/' + sim.locale, '_blank' );
-            phetWindow.focus();
+            phetWindow && phetWindow.focus();
           }
         },
 
@@ -195,7 +195,7 @@ define( function( require ) {
 
           if ( !fuzzes ) {
             var reportWindow = window.open( url, '_blank' );
-            reportWindow.focus();
+            reportWindow && reportWindow.focus();
           }
         },
         tagName: 'button'
@@ -206,7 +206,7 @@ define( function( require ) {
         callback: function() {
           if ( !fuzzes ) {
             var win = window.open( 'http://api.qrserver.com/v1/create-qr-code/?data=' + encodeURIComponent( window.location.href ) + '&size=220x220&margin=0', '_blank' );
-            win.focus();
+            win && win.focus();
           }
         },
 
