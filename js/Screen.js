ca8a47c73a9fb5f8a4cdeb665e439aa02126fa9c
Chris Malley <cmalley@pixelzoom.com>
2019-10-07 19:42:00 -0600

    break long lines, remove html markup

--- js/Screen.js
+++ js/Screen.js
@@ -3,10 +3,10 @@
 /**
  * A Screen is the largest chunk of a simulation. (Java sims used the term Module, but that term
  * is too overloaded to use with JavaScript and Git.)
- * <p>
- * When creating a Sim, Screens are supplied as the arguments. They can be specified as object literals or through instances of this class.
- * This class may centralize default behavior or state for Screens in the future, but right now it only allows you to create
- * Sims without using named parameter object literals.
+ *
+ * When creating a Sim, Screens are supplied as the arguments. They can be specified as object literals or through
+ * instances of this class. This class may centralize default behavior or state for Screens in the future, but right
+ * now it only allows you to create Sims without using named parameter object literals.
  *
  * @author Sam Reid (PhET Interactive Simulations)
  */
