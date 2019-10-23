4932b51bee027e8858453dc7b635e2633b353e67
Jonathan Olson <jonathan.olson@colorado.edu>
2019-10-23 14:07:23 -0600

    Adding `user-select: none` CSS style to the aria-live div, see https://github.com/phetsims/scenery/issues/1006

--- js/Sim.js
+++ js/Sim.js
@@ -21,6 +21,7 @@ define( function( require ) {
   var Display = require( 'SCENERY/display/Display' );
   var DotUtil = require( 'DOT/Util' );// eslint-disable-line
   var Emitter = require( 'AXON/Emitter' );
+  var Features = require( 'SCENERY/util/Features' );
   var HomeScreen = require( 'JOIST/HomeScreen' );
   var HomeScreenView = require( 'JOIST/HomeScreenView' );
   var inherit = require( 'PHET_CORE/inherit' );
@@ -298,6 +299,13 @@ define( function( require ) {
 
     // prevent scrollbars
     $body.css( 'padding', '0' ).css( 'margin', '0' ).css( 'overflow', 'hidden' );
+
+    // set `user-select: none` on the aria-live container to prevent iOS text selection issue, see
+    // https://github.com/phetsims/scenery/issues/1006
+    var ariaLiveContainer = document.getElementById( 'aria-live-elements' );
+    if ( ariaLiveContainer ) {
+      ariaLiveContainer.style[ Features.userSelect ] = 'none';
+    }
 
     // check to see if the sim div already exists in the DOM under the body. This is the case for https://github.com/phetsims/scenery/issues/174 (iOS offline reading list)
     if ( document.getElementById( 'sim' ) && document.getElementById( 'sim' ).parentNode === document.body ) {
