4e9f7bb1d2f6eadfd77deca99823f69ff7318ead
samreid <reids@colorado.edu>
2020-03-11 10:16:01 -0600

    Convert HomeScreen to ES6 class and address REVIEW comments, see https://github.com/phetsims/joist/issues/602

--- js/HomeScreen.js
+++ js/HomeScreen.js
@@ -7,7 +7,6 @@
  */
 
 import Property from '../../axon/js/Property.js';
-import inherit from '../../phet-core/js/inherit.js';
 import merge from '../../phet-core/js/merge.js';
 import HomeScreenModel from './HomeScreenModel.js';
 import HomeScreenView from './HomeScreenView.js';
@@ -17,48 +16,43 @@ import Screen from './Screen.js';
 // constants
 const BACKGROUND_COLOR = 'black';
 
-/**
- * REVIEW: Are we avoiding @param here? https://github.com/phetsims/joist/issues/602
- * {string} simName
- * {function():Property.<Screen>} getScreenProperty - at the time of construction, the Sim.screenProperty is not yet
- *                                                  - assigned (because it may itself include the HomeScreen), so we
- *                                                  - must use a function to lazily get it after it is assigned
- * REVIEW: What is the downside of creating the screenProperty before creating the HomeScreen? This seems like a lot
- * REVIEW: of work to avoid that. See https://github.com/phetsims/joist/issues/602
- * {Screen[]} simScreens
- * {Tandem} tandem
- * (options} [options]
- * @constructor
- */
-function HomeScreen( simName, getScreenProperty, simScreens, tandem, options ) {
+class HomeScreen extends Screen {
 
-  options = merge( {
+  /**
+   * @param {string} simName
+   * @param {function():Property.<Screen>} getScreenProperty - at the time of construction, the Sim.screenProperty is
+   *                                                         - not yet assigned (because it may itself include the
+   *                                                         - HomeScreen), so we must use a function to lazily get it
+   *                                                         - after it is assigned
+   * @param {Screen[]} simScreens
+   * @param {Tandem} tandem
+   * @param {Object} [options]
+   * @constructor
+   */
+  constructor( simName, getScreenProperty, simScreens, tandem, options ) {
 
-    //TODO get this color from LookAndFeel, see https://github.com/phetsims/joist/issues/255
-    backgroundColorProperty: new Property( BACKGROUND_COLOR )
-  }, options );
+    options = merge( {
 
-  assert && assert( !options.tandem, 'tandem is a required constructor parameter, not an option' );
-  options.tandem = tandem;
+      //TODO get this color from LookAndFeel, see https://github.com/phetsims/joist/issues/255
+      backgroundColorProperty: new Property( BACKGROUND_COLOR )
+    }, options );
 
-  Screen.call( this,
+    assert && assert( !options.tandem, 'tandem is a required constructor parameter, not an option' );
+    options.tandem = tandem;
 
-    // createModel
-    () => new HomeScreenModel( getScreenProperty(), simScreens, tandem.createTandem( 'model' ) ),
-
-    // createView
-    model => new HomeScreenView( simName, model, tandem.createTandem( 'view' ), _.pick( options, [
-      'warningNode'
-    ] ) ),
-
-    options
-  );
+    super(
+      () => new HomeScreenModel( getScreenProperty(), simScreens, tandem.createTandem( 'model' ) ),
+      model => new HomeScreenView( simName, model, tandem.createTandem( 'view' ), _.pick( options, [
+        'warningNode'
+      ] ) ),
+      options
+    );
+  }
 }
+
+// @public (read-only)
+HomeScreen.BACKGROUND_COLOR = BACKGROUND_COLOR;
 
 joist.register( 'HomeScreen', HomeScreen );
 
-export default inherit( Screen, HomeScreen, {}, {
-
-  // @public
-  BACKGROUND_COLOR: BACKGROUND_COLOR
-} );
+export default HomeScreen;
