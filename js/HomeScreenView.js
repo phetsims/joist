e263dba85124bf2d14e69e875e1995b4acf2a6de
Jonathan Olson <jonathan.olson@colorado.edu>
2020-04-13 11:02:47 -0600

    Switching lazyLink to link for title, see refactoring for https://github.com/phetsims/scenery/issues/1044

--- js/HomeScreenView.js
+++ js/HomeScreenView.js
@@ -72,7 +72,7 @@ function HomeScreenView( simName, model, tandem, options ) {
   } );
 
   // Have this before adding the child to support the startup layout.
-  title.boundsProperty.lazyLink( () => {
+  title.boundsProperty.link( () => {
     title.centerX = this.layoutBounds.centerX;
   } );
 
