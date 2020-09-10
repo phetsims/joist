a07bacaa8eab3e783b7cdad24f0463bca2e7763e
pixelzoom <cmalley@pixelzoom.com>
2020-09-10 17:18:18 -0600

    convert HomeButton to ES6, https://github.com/phetsims/tasks/issues/1044

--- js/HomeButton.js
+++ js/HomeButton.js
@@ -8,7 +8,6 @@
 
 import Property from '../../axon/js/Property.js';
 import Shape from '../../kite/js/Shape.js';
-import inherit from '../../phet-core/js/inherit.js';
 import merge from '../../phet-core/js/merge.js';
 import FocusHighlightPath from '../../scenery/js/accessibility/FocusHighlightPath.js';
 import Node from '../../scenery/js/nodes/Node.js';
@@ -22,61 +21,60 @@ import joistStrings from './joistStrings.js';
 // constants
 const homeScreenDescriptionString = joistStrings.a11y.homeScreenDescription;
 
-/**
- * @param {number} navBarHeight
- * @param {Property.<string>} navigationBarFillProperty - the color of the navbar, as a string.
- * @param {Tandem} tandem
- * @param {Property.<string|null>} pdomDisplayNameProperty - for the HomeScreen, for description
- * @param {Object} [options]
- * @constructor
- */
-function HomeButton( navBarHeight, navigationBarFillProperty, pdomDisplayNameProperty, tandem, options ) {
+class HomeButton extends JoistButton {
+  /**
+   * @param {number} navBarHeight
+   * @param {Property.<string>} navigationBarFillProperty - the color of the navbar, as a string.
+   * @param {Tandem} tandem
+   * @param {Property.<string|null>} pdomDisplayNameProperty - for the HomeScreen, for description
+   * @param {Object} [options]
+   */
+  constructor( navBarHeight, navigationBarFillProperty, pdomDisplayNameProperty, tandem, options ) {
 
-  options = merge( {
-    highlightExtensionWidth: 4,
-    listener: null,
+    options = merge( {
+      highlightExtensionWidth: 4,
+      listener: null,
 
-    // pdom,
-    tagName: 'button',
-    containerTagName: 'li',
-    descriptionContent: homeScreenDescriptionString,
-    appendDescription: true
-  }, options );
+      // pdom,
+      tagName: 'button',
+      containerTagName: 'li',
+      descriptionContent: homeScreenDescriptionString,
+      appendDescription: true
+    }, options );
 
-  assert && assert( !options.innerContent, 'HomeButton sets its own innerContent' );
+    assert && assert( !options.innerContent, 'HomeButton sets its own innerContent' );
 
-  const homeIcon = new FontAwesomeNode( 'home' );
-  // scale so that the icon is slightly taller than screen button icons, value determined empirically, see joist#127
-  homeIcon.setScaleMagnitude( 0.48 * navBarHeight / homeIcon.height );
+    const homeIcon = new FontAwesomeNode( 'home' );
+    // scale so that the icon is slightly taller than screen button icons, value determined empirically, see joist#127
+    homeIcon.setScaleMagnitude( 0.48 * navBarHeight / homeIcon.height );
 
-  // transparent background, size determined empirically so that highlight is the same size as highlight on screen buttons
-  const background = new Rectangle( 0, 0, homeIcon.width + 12, navBarHeight );
-  homeIcon.center = background.center;
+    // transparent background, size determined empirically so that highlight is the same size as highlight on screen buttons
+    const background = new Rectangle( 0, 0, homeIcon.width + 12, navBarHeight );
+    homeIcon.center = background.center;
 
-  const content = new Node( { children: [ background, homeIcon ] } );
+    const content = new Node( { children: [ background, homeIcon ] } );
 
-  JoistButton.call( this, content, navigationBarFillProperty, tandem, options );
+    super( content, navigationBarFillProperty, tandem, options );
 
-  // pdom - Pass a shape to the focusHighlight to prevent dilation, then tweak the bottom up just a hair so it
-  // isn't off the screen.
-  const highlightLineWidth = FocusHighlightPath.getOuterLineWidthFromNode( this );
-  this.focusHighlight = Shape.bounds( this.bounds.setMaxY( this.bounds.maxY - highlightLineWidth / 2 ) );
+    // pdom - Pass a shape to the focusHighlight to prevent dilation, then tweak the bottom up just a hair so it
+    // isn't off the screen.
+    const highlightLineWidth = FocusHighlightPath.getOuterLineWidthFromNode( this );
+    this.focusHighlight = Shape.bounds( this.bounds.setMaxY( this.bounds.maxY - highlightLineWidth / 2 ) );
 
-  Property.multilink( [ this.interactionStateProperty, navigationBarFillProperty ], function( interactionState, navigationBarFill ) {
-    if ( navigationBarFill === 'black' ) {
-      homeIcon.fill = interactionState === ButtonInteractionState.PRESSED ? 'gray' : 'white';
-    }
-    else {
-      homeIcon.fill = interactionState === ButtonInteractionState.PRESSED ? '#444' : '#222';
-    }
-  } );
+    Property.multilink( [ this.interactionStateProperty, navigationBarFillProperty ], function( interactionState, navigationBarFill ) {
+      if ( navigationBarFill === 'black' ) {
+        homeIcon.fill = interactionState === ButtonInteractionState.PRESSED ? 'gray' : 'white';
+      }
+      else {
+        homeIcon.fill = interactionState === ButtonInteractionState.PRESSED ? '#444' : '#222';
+      }
+    } );
 
-  pdomDisplayNameProperty.link( name => {
-    this.innerContent = name;
-  } );
+    pdomDisplayNameProperty.link( name => {
+      this.innerContent = name;
+    } );
+  }
 }
 
 joist.register( 'HomeButton', HomeButton );
-
-inherit( JoistButton, HomeButton );
 export default HomeButton;
