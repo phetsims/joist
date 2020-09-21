697693e0d1082697b2ea18ae6be5cc91e95efd96
pixelzoom <cmalley@pixelzoom.com>
2020-09-20 23:17:11 -0600

    convert LookAndFeel to ES6, https://github.com/phetsims/tasks/issues/1044

--- js/LookAndFeel.js
+++ js/LookAndFeel.js
@@ -8,46 +8,40 @@
 
 import DerivedProperty from '../../axon/js/DerivedProperty.js';
 import Property from '../../axon/js/Property.js';
-import inherit from '../../phet-core/js/inherit.js';
 import Color from '../../scenery/js/util/Color.js';
 import joist from './joist.js';
 
-/**
- *
- * @constructor
- */
-function LookAndFeel() {
+class LookAndFeel {
 
-  // @public background color for the currently selected screen, which will be set on the Display as its backgroundColor
-  this.backgroundColorProperty = new Property( 'black' );
+  constructor() {
 
-  // @public (joist-internal) {boolean} - True if the navigation bar background is black
-  this.navigationBarDarkProperty = new DerivedProperty( [ this.backgroundColorProperty ],
-    function( backgroundColor ) {
-      return !!new Color( backgroundColor ).equals( Color.BLACK );
-    } );
+    // @public background color for the currently selected screen, which will be set on the Display as its backgroundColor
+    this.backgroundColorProperty = new Property( 'black' );
 
-  // @public (joist-internal) - Navigation bar background fill
-  this.navigationBarFillProperty = new DerivedProperty( [ this.navigationBarDarkProperty ],
-    function( backgroundDark ) {
-      return backgroundDark ? 'white' : 'black';
-    } );
+    // @public (joist-internal) {boolean} - True if the navigation bar background is black
+    this.navigationBarDarkProperty = new DerivedProperty( [ this.backgroundColorProperty ],
+      function( backgroundColor ) {
+        return !!new Color( backgroundColor ).equals( Color.BLACK );
+      } );
 
-  // @public (joist-internal) - Navigation bar text fill
-  this.navigationBarTextFillProperty = new DerivedProperty( [ this.navigationBarFillProperty ],
-    function( navigationBarFill ) {
-      return navigationBarFill === 'black' ? 'white' : 'black';
-    } );
+    // @public (joist-internal) - Navigation bar background fill
+    this.navigationBarFillProperty = new DerivedProperty( [ this.navigationBarDarkProperty ],
+      function( backgroundDark ) {
+        return backgroundDark ? 'white' : 'black';
+      } );
+
+    // @public (joist-internal) - Navigation bar text fill
+    this.navigationBarTextFillProperty = new DerivedProperty( [ this.navigationBarFillProperty ],
+      function( navigationBarFill ) {
+        return navigationBarFill === 'black' ? 'white' : 'black';
+      } );
+  }
+
+  // @public
+  reset() {
+    this.backgroundColorProperty.reset();
+  }
 }
 
 joist.register( 'LookAndFeel', LookAndFeel );
-
-inherit( Object, LookAndFeel, {
-
-  // @public
-  reset: function() {
-    this.backgroundColorProperty.reset();
-  }
-} );
-
 export default LookAndFeel;
