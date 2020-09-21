61c1fcfa8e704d752cdaf770f32c54004a829a22
pixelzoom <cmalley@pixelzoom.com>
2020-09-20 22:18:48 -0600

    break long line

--- js/A11yButtonsHBox.js
+++ js/A11yButtonsHBox.js
@@ -55,8 +55,8 @@ function A11yButtonsHBox( sim, backgroundColorProperty, tandem, options ) {
       tandem.createTandem( 'keyboardHelpButton' )
     );
 
-    // only show the keyboard help button if the sim supports interactive descriptions, there is keyboard help content, and we are
-    // not in mobile safari
+    // only show the keyboard help button if the sim supports interactive descriptions, there is keyboard help content,
+    // and we are not in mobile safari
     if ( sim.supportsInteractiveDescriptions && !platform.mobileSafari ) {
       a11yButtons.push( keyboardHelpButton );
     }
