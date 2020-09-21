5f470056d1d146bb6f2814396933615f3c54c088
pixelzoom <cmalley@pixelzoom.com>
2020-09-20 22:35:34 -0600

    convert KeyboardHelpDialog to ES6, https://github.com/phetsims/tasks/issues/1044

--- js/KeyboardHelpDialog.js
+++ js/KeyboardHelpDialog.js
@@ -7,7 +7,6 @@
  * @author Jesse Greenberg
  */
 
-import inherit from '../../phet-core/js/inherit.js';
 import merge from '../../phet-core/js/merge.js';
 import KeyboardHelpSection from '../../scenery-phet/js/keyboard/help/KeyboardHelpSection.js';
 import TabKeyNode from '../../scenery-phet/js/keyboard/TabKeyNode.js';
@@ -24,84 +23,77 @@ import joistStrings from './joistStrings.js';
 // constants
 const TITLE_MAX_WIDTH = 670;
 
-// string
-const keyboardShortcutsTitleString = joistStrings.keyboardShortcuts.title;
-const keyboardShortcutsToGetStartedString = joistStrings.keyboardShortcuts.toGetStarted;
+class KeyboardHelpDialog extends Dialog {
 
-// pdom
-const hotKeysAndHelpString = joistStrings.a11y.keyboardHelp.hotKeysAndHelp;
-const tabToGetStartedString = joistStrings.a11y.keyboardHelp.tabToGetStarted;
+  /**
+   * @param {Node} helpContent - a node containing the sim specific keyboard help content
+   * @param {Object} [options]
+   */
+  constructor( helpContent, options ) {
 
-/**
- * @param {Node} helpContent - a node containing the sim specific keyboard help content
- * @param {Object} [options]
- * @constructor
- */
-function KeyboardHelpDialog( helpContent, options ) {
+    options = merge( {
+      titleAlign: 'center',
+      fill: 'rgb( 214, 237, 249 )',
+      ySpacing: 15,
+      tandem: Tandem.REQUIRED,
 
-  options = merge( {
-    titleAlign: 'center',
-    fill: 'rgb( 214, 237, 249 )',
-    ySpacing: 15,
-    tandem: Tandem.REQUIRED,
+      phetioReadOnly: true, // the KeyboardHelpDialog should not be settable
+      phetioDynamicElement: true,
 
-    phetioReadOnly: true, // the KeyboardHelpDialog should not be settable
-    phetioDynamicElement: true,
+      // Because of the special titleNode, we set the aria-labelledby attribute manually; see below.
+      addAriaLabelledByFromTitle: false
+    }, options );
 
-    // Because of the special titleNode, we set the aria-labelledby attribute manually; see below.
-    addAriaLabelledByFromTitle: false
-  }, options );
+    // title
+    assert && assert( !options.title, 'KeyboardHelpDialog sets title' );
+    const shortcutsTitleText = new Text( joistStrings.keyboardShortcuts.title, {
+      font: new PhetFont( {
+        weight: 'bold',
+        size: 24
+      } ),
+      maxWidth: TITLE_MAX_WIDTH,
 
-  // title
-  assert && assert( !options.title, 'KeyboardHelpDialog sets title' );
-  const shortcutsTitleText = new Text( keyboardShortcutsTitleString, {
-    font: new PhetFont( {
-      weight: 'bold',
-      size: 24
-    } ),
-    maxWidth: TITLE_MAX_WIDTH,
+      // pdom options
+      tagName: 'h1',
+      innerContent: joistStrings.a11y.keyboardHelp.hotKeysAndHelp
+    } );
 
-    // pdom options
-    tagName: 'h1',
-    innerContent: hotKeysAndHelpString
-  } );
+    // a line to say "tab to get started" below the "Keyboard Shortcuts" 'title'
+    const tabHintLine = KeyboardHelpSection.labelWithIcon( joistStrings.keyboardShortcuts.toGetStarted,
+      new TabKeyNode(), joistStrings.a11y.keyboardHelp.tabToGetStarted, {
+        iconOptions: {
+          tagName: 'p' // because there is only one, and the default is an li tag
+        }
+      } );
 
-  // a line to say "tab to get started" below the "Keyboard Shortcuts" 'title'
-  const tabHintLine = KeyboardHelpSection.labelWithIcon( keyboardShortcutsToGetStartedString, new TabKeyNode(), tabToGetStartedString, {
-    iconOptions: {
-      tagName: 'p' // because there is only one, and the default is an li tag
-    }
-  } );
+    // stack the two items with a bit of spacing
+    options.title = new VBox( {
+        children: [
+          shortcutsTitleText,
 
-  // stack the two items with a bit of spacing
-  options.title = new VBox( {
-      children: [
-        shortcutsTitleText,
+          // labelWithIcon is meant to be passed to KeyboardHelpSection, so we have to hack a bit here
+          new HBox( { children: [ tabHintLine.icon, tabHintLine.label ], spacing: 4 } )
+        ],
+        spacing: 5,
 
-        // labelWithIcon is meant to be passed to KeyboardHelpSection, so we have to hack a bit here
-        new HBox( { children: [ tabHintLine.icon, tabHintLine.label ], spacing: 4 } )
-      ],
-      spacing: 5,
+        // pdom
+        tagName: 'div'
+      }
+    );
 
-      // pdom
-      tagName: 'div'
-    }
-  );
+    // help content surrounded by a div unless already specified, so that all content is read when dialog opens
+    helpContent.tagName = helpContent.tagName || 'div';
 
-  // help content surrounded by a div unless already specified, so that all content is read when dialog opens
-  helpContent.tagName = helpContent.tagName || 'div';
+    super( helpContent, options );
 
-  Dialog.call( this, helpContent, options );
-
-  // (a11y) Make sure that the title passed to the Dialog has an accessible name.
-  this.addAriaLabelledbyAssociation( {
-    thisElementName: PDOMPeer.PRIMARY_SIBLING,
-    otherNode: shortcutsTitleText,
-    otherElementName: PDOMPeer.PRIMARY_SIBLING
-  } );
+    // (a11y) Make sure that the title passed to the Dialog has an accessible name.
+    this.addAriaLabelledbyAssociation( {
+      thisElementName: PDOMPeer.PRIMARY_SIBLING,
+      otherNode: shortcutsTitleText,
+      otherElementName: PDOMPeer.PRIMARY_SIBLING
+    } );
+  }
 }
 
 joist.register( 'KeyboardHelpDialog', KeyboardHelpDialog );
-
-inherit( Dialog, KeyboardHelpDialog );
 export default KeyboardHelpDialog;
