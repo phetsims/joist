64c4e5f716fa27eb59c4f3093c2dbfb932e45545
pixelzoom <cmalley@pixelzoom.com>
2020-09-20 22:32:41 -0600

    convert KeyboardHelpButton to ES6, https://github.com/phetsims/tasks/issues/1044

--- js/KeyboardHelpButton.js
+++ js/KeyboardHelpButton.js
@@ -7,7 +7,6 @@
  * @author Jesse Greenberg
  */
 
-import inherit from '../../phet-core/js/inherit.js';
 import merge from '../../phet-core/js/merge.js';
 import Image from '../../scenery/js/nodes/Image.js';
 import DialogIO from '../../sun/js/DialogIO.js';
@@ -17,8 +16,8 @@ import PhetioObject from '../../tandem/js/PhetioObject.js';
 import darkIconImage from '../images/keyboard-icon-on-white_png.js'; // on a white navbar
 import brightIconImage from '../images/keyboard-icon_png.js'; // on a black navbar
 import joist from './joist.js';
-import joistStrings from './joistStrings.js';
 import JoistButton from './JoistButton.js';
+import joistStrings from './joistStrings.js';
 import KeyboardHelpDialog from './KeyboardHelpDialog.js';
 
 // constants
@@ -26,69 +25,69 @@ const hotKeysAndHelpString = joistStrings.a11y.keyboardHelp.hotKeysAndHelp;
 const HELP_BUTTON_HEIGHT = 67;
 const HELP_BUTTON_SCALE = 0.30;  // scale applied to the icon
 
-/**
- * @param {Node} helpContent - content for the KeyboardHelpDialog
- * @param {Property.<Color|string>} backgroundColorProperty
- * @param {Tandem} tandem
- * @param {Object} [options]
- * @constructor
- */
-function KeyboardHelpButton( helpContent, backgroundColorProperty, tandem, options ) {
+class KeyboardHelpButton extends JoistButton {
 
-  options = merge( {
-    highlightExtensionWidth: 5,
-    highlightExtensionHeight: 10,
+  /**
+   * @param {Node} helpContent - content for the KeyboardHelpDialog
+   * @param {Property.<Color|string>} backgroundColorProperty
+   * @param {Tandem} tandem
+   * @param {Object} [options]
+   */
+  constructor( helpContent, backgroundColorProperty, tandem, options ) {
 
-    // The keyboard button is not vertically symmetric, due to the cable on the top.
-    // This offset adjusts the body of the keyboard to be in the center, so it
-    // will align with the speaker button and the PhET logo
-    highlightCenterOffsetY: 2,
+    options = merge( {
+      highlightExtensionWidth: 5,
+      highlightExtensionHeight: 10,
 
-    // pdom
-    tagName: 'button',
-    innerContent: hotKeysAndHelpString
-  }, options );
+      // The keyboard button is not vertically symmetric, due to the cable on the top.
+      // This offset adjusts the body of the keyboard to be in the center, so it
+      // will align with the speaker button and the PhET logo
+      highlightCenterOffsetY: 2,
 
-  assert && assert( !options.listener, 'KeyboardHelpButton set\'s its own listener' );
+      // pdom
+      tagName: 'button',
+      innerContent: hotKeysAndHelpString
+    }, options );
 
-  PhetioObject.mergePhetioComponentOptions( { visibleProperty: { phetioFeatured: true } }, options );
+    assert && assert( !options.listener, 'KeyboardHelpButton set\'s its own listener' );
 
-  const keyboardHelpDialogCapsule = new PhetioCapsule( tandem => {
-    return new KeyboardHelpDialog( helpContent, {
-      focusOnCloseNode: this,
-      tandem: tandem
+    PhetioObject.mergePhetioComponentOptions( { visibleProperty: { phetioFeatured: true } }, options );
+
+    const keyboardHelpDialogCapsule = new PhetioCapsule( tandem => {
+      return new KeyboardHelpDialog( helpContent, {
+        focusOnCloseNode: this,
+        tandem: tandem
+      } );
+    }, [], {
+      tandem: tandem.createTandem( 'keyboardHelpDialogCapsule' ),
+      phetioType: PhetioCapsuleIO( DialogIO )
     } );
-  }, [], {
-    tandem: tandem.createTandem( 'keyboardHelpDialogCapsule' ),
-    phetioType: PhetioCapsuleIO( DialogIO )
-  } );
 
-  options.listener = () => {
-    const keyboardHelpDialog = keyboardHelpDialogCapsule.getElement();
-    keyboardHelpDialog.show();
+    options.listener = () => {
+      const keyboardHelpDialog = keyboardHelpDialogCapsule.getElement();
+      keyboardHelpDialog.show();
 
-    // if listener was fired because of accessibility
-    if ( this.buttonModel.isA11yClicking() ) {
+      // if listener was fired because of accessibility
+      if ( this.buttonModel.isA11yClicking() ) {
 
-      // focus the close button if the dialog is open with a keyboard
-      keyboardHelpDialog.focusCloseButton();
-    }
-  };
+        // focus the close button if the dialog is open with a keyboard
+        keyboardHelpDialog.focusCloseButton();
+      }
+    };
 
-  const icon = new Image( brightIconImage, {
-    scale: HELP_BUTTON_SCALE / brightIconImage.height * HELP_BUTTON_HEIGHT,
-    pickable: false
-  } );
+    const icon = new Image( brightIconImage, {
+      scale: HELP_BUTTON_SCALE / brightIconImage.height * HELP_BUTTON_HEIGHT,
+      pickable: false
+    } );
 
-  JoistButton.call( this, icon, backgroundColorProperty, tandem, options );
+    super( icon, backgroundColorProperty, tandem, options );
 
-  // change the icon so that it is visible when the background changes from dark to light
-  backgroundColorProperty.link( function( backgroundColor ) {
-    icon.image = backgroundColor === 'black' ? brightIconImage : darkIconImage;
-  } );
+    // change the icon so that it is visible when the background changes from dark to light
+    backgroundColorProperty.link( function( backgroundColor ) {
+      icon.image = backgroundColor === 'black' ? brightIconImage : darkIconImage;
+    } );
+  }
 }
 
 joist.register( 'KeyboardHelpButton', KeyboardHelpButton );
-
-inherit( JoistButton, KeyboardHelpButton );
 export default KeyboardHelpButton;
