a8de90d1e0ca44d0b4d523fa74a6d61883a7bd6d
pixelzoom <cmalley@pixelzoom.com>
2020-09-22 15:10:38 -0600

    convert DialogsScreenView to https://github.com/phetsims/tasks/issues/1044

--- js/demo/DialogsScreenView.js
+++ js/demo/DialogsScreenView.js
@@ -6,7 +6,6 @@
  * @author Chris Malley (PixelZoom, Inc.)
  */
 
-import inherit from '../../../phet-core/js/inherit.js';
 import GeneralKeyboardHelpSection from '../../../scenery-phet/js/keyboard/help/GeneralKeyboardHelpSection.js';
 import VBox from '../../../scenery/js/nodes/VBox.js';
 import Panel from '../../../sun/js/Panel.js';
@@ -15,38 +14,35 @@ import joist from '../joist.js';
 import KeyboardHelpButton from '../KeyboardHelpButton.js';
 import ScreenView from '../ScreenView.js';
 
-/**
- * @constructor
- */
-function DialogsScreenView() {
+class DialogsScreenView extends ScreenView {
+  constructor() {
 
-  ScreenView.call( this );
+    super();
 
-  const keyboardHelpDialogContent = new GeneralKeyboardHelpSection();
+    const keyboardHelpDialogContent = new GeneralKeyboardHelpSection();
 
-  const keyboardHelpButton = new KeyboardHelpButton(
-    keyboardHelpDialogContent,
-    phet.joist.sim.lookAndFeel.backgroundColorProperty,
-    Tandem.GENERAL.createTandem( 'keyboardHelpButton' )
-  );
-  keyboardHelpButton.setScaleMagnitude( 2 );
+    const keyboardHelpButton = new KeyboardHelpButton(
+      keyboardHelpDialogContent,
+      phet.joist.sim.lookAndFeel.backgroundColorProperty,
+      Tandem.GENERAL.createTandem( 'keyboardHelpButton' )
+    );
+    keyboardHelpButton.setScaleMagnitude( 2 );
 
-  // Since KeyboardHelpButton adapts its color to the navigation bar,
-  // put the button in a panel that's the same color as the navigation bar
-  const keyboardHelpPanel = new Panel( keyboardHelpButton, {
-    fill: phet.joist.sim.lookAndFeel.navigationBarFillProperty.value
-  } );
+    // Since KeyboardHelpButton adapts its color to the navigation bar,
+    // put the button in a panel that's the same color as the navigation bar
+    const keyboardHelpPanel = new Panel( keyboardHelpButton, {
+      fill: phet.joist.sim.lookAndFeel.navigationBarFillProperty.value
+    } );
 
-  this.addChild( new VBox( {
-    children: [
-      keyboardHelpPanel
-    ],
-    spacing: 20,
-    center: this.layoutBounds.center
-  } ) );
+    this.addChild( new VBox( {
+      children: [
+        keyboardHelpPanel
+      ],
+      spacing: 20,
+      center: this.layoutBounds.center
+    } ) );
+  }
 }
 
 joist.register( 'DialogsScreenView', DialogsScreenView );
-
-inherit( ScreenView, DialogsScreenView );
 export default DialogsScreenView;
