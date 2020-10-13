d243c248622d1ec0988e623a6c13d46b1806b76b
pixelzoom <cmalley@pixelzoom.com>
2020-10-13 10:33:56 -0600

    convert to ES6 class, https://github.com/phetsims/tasks/issues/1044

--- js/thirdPartySupport/LegendsOfLearningSupport.js
+++ js/thirdPartySupport/LegendsOfLearningSupport.js
@@ -1,45 +1,47 @@
 // Copyright 2017-2020, University of Colorado Boulder
 
 /**
- * Support for Legends of Learning platform. Sends init message after sim is constructed and support pause/resume.
+ * Support for Legends of Learning platform. Sends init message after sim is constructed and supports pause/resume.
+ *
+ * To test this class, follow the 'Legends of Learning Test' instructions at
+ * https://github.com/phetsims/QA/blob/master/documentation/qa-book.md#legends-of-learning-test
  *
  * @author Sam Reid (PhET Interactive Simulations)
  */
 
-import inherit from '../../../phet-core/js/inherit.js';
 import joist from '../joist.js';
 
-/**
- * @param {Sim} sim
- * @constructor
- */
-function LegendsOfLearningSupport( sim ) {
+class LegendsOfLearningSupport {
 
-  // @private
-  this.sim = sim;
+  /**
+   * @param {Sim} sim
+   */
+  constructor( sim ) {
 
-  // Respond to pause/resume commands from the Legends of Learning platform
-  window.addEventListener( 'message', function( message ) {
-    if ( message.data.messageName === 'pause' ) {
-      sim.stepOneFrame();
-      sim.activeProperty.value = false;
-    }
-    else if ( message.data.messageName === 'resume' ) {
-      sim.activeProperty.value = true;
-    }
-  } );
-}
+    // @private
+    this.sim = sim;
 
-joist.register( 'LegendsOfLearningSupport', LegendsOfLearningSupport );
+    // Respond to pause/resume commands from the Legends of Learning platform
+    window.addEventListener( 'message', function( message ) {
+      if ( message.data.messageName === 'pause' ) {
+        sim.stepOneFrame();
+        sim.activeProperty.value = false;
+      }
+      else if ( message.data.messageName === 'resume' ) {
+        sim.activeProperty.value = true;
+      }
+    } );
+  }
 
-inherit( Object, LegendsOfLearningSupport, {
-  start: function() {
+  // @public
+  start() {
 
     // Send init message when sim has started up so that Legends of Learning can remove their splash screen
     this.sim.isConstructionCompleteProperty.link( isConstructionComplete => {
       isConstructionComplete && ( window.parent !== window ) && window.parent.postMessage( { message: 'init' }, '*' );
     } );
   }
-} );
+}
 
+joist.register( 'LegendsOfLearningSupport', LegendsOfLearningSupport );
 export default LegendsOfLearningSupport;
