9e86812c4d98f6f5a53e3cc1cbc3ecb26e559bee
zepumph <michael.kauzmann@colorado.edu>
2021-01-22 12:45:15 -0900

    factor out asyncLoader from simLauncher, https://github.com/phetsims/tambo/issues/126 https://github.com/phetsims/joist/issues/687

--- js/simLauncher.js
+++ js/simLauncher.js
@@ -1,80 +1,52 @@
-// Copyright 2013-2020, University of Colorado Boulder
+// Copyright 2013-2021, University of Colorado Boulder
 /**
- * Singleton which launches a PhET Simulation, after preloading resources such as images or dynamic modules.
+ * Singleton which launches a PhET Simulation, after using PHET_CORE/asyncLoader to make sure resources such as images,
+ * sounds, or dynamic modules have finished loading.
  *
  * @author Sam Reid (PhET Interactive Simulations)
  */
 
-import arrayRemove from '../../phet-core/js/arrayRemove.js';
+import asyncLoader from '../../phet-core/js/asyncLoader.js';
 import Tandem from '../../tandem/js/Tandem.js';
 import joist from './joist.js';
 
 // See below for dynamic imports, which must be locked.
 let phetioEngine = null; // {null|PhetioEngine}
 
-// Dynamic imports must be started after simLauncher is ready to process locks, hence the closure.
-const initializeDynamicImports = simLauncher => {
+const unlockBrand = asyncLoader.createLock( { name: 'brand' } );
+import( /* webpackMode: "eager" */ `../../brand/${phet.chipper.brand}/js/Brand.js` ).then( module => unlockBrand() );
 
-  const unlockBrand = simLauncher.createLock( { name: 'brand' } );
-  import( /* webpackMode: "eager" */ `../../brand/${phet.chipper.brand}/js/Brand.js`).then( module => unlockBrand() );
-
-  if ( Tandem.PHET_IO_ENABLED ) {
-    const unlockPhetioEngine = simLauncher.createLock( { name: 'phetioEngine' } );
-    import( /* webpackMode: "eager" */ '../../phet-io/js/phetioEngine.js').then( module => {
-      phetioEngine = module.default;
-      unlockPhetioEngine();
-    } );
-  }
-};
+if ( Tandem.PHET_IO_ENABLED ) {
+  const unlockPhetioEngine = asyncLoader.createLock( { name: 'phetioEngine' } );
+  import( /* webpackMode: "eager" */ '../../phet-io/js/phetioEngine.js' ).then( module => {
+    phetioEngine = module.default;
+    unlockPhetioEngine();
+  } );
+}
 
 class SimLauncher {
   constructor() {
 
-    // @private {Array.<*>} - Locks waiting to be resolved before we can move to the next phase of the launch sequence.
-    // Lock objects can be arbitrary objects.
-    this.pendingLocks = [];
-
-    // @private {boolean} - Marked as true when there are no more locks and we try to proceed.  Helps protect against
-    // new locks being created after they should be.
-    this.launchBegan = false;
-
     // @private {boolean} - Marked as true when simLauncher has finished its work cycle and control is given over to the
     // simulation to finish initialization.
     this.launchComplete = false;
-
-    // @private {function} - The callback which should be invoked on launch.
-    this.callback = null;
   }
 
   /**
    * Launch the Sim by preloading the images and calling the callback.
    * @public - to be called by main()s everywhere
    *
-   * @param {function} callback - the callback function which should create and start the sim, given that the images
-   *                              are loaded
+   * @param {function} callback - the callback function which should create and start the sim, given that all async
+   *                              content is loaded
    */
   launch( callback ) {
-    assert && assert( !window.phet.launchCalled, 'Tried to launch twice' );
+    assert && assert( !window.phet.joist.launchCalled, 'Tried to launch twice' );
 
-    this.callback = callback;
-
-    // Signify that the simLauncher was called, see https://github.com/phetsims/joist/issues/142
-    window.phet.joist.launchCalled = true;
-  }
-
-  /**
-   * Attempts to proceed to the next phase if possible (otherwise it's a no-op).
-   * @private
-   */
-  proceedIfReady() {
-
-    assert && assert( !this.launchComplete, 'cannot proceed if already launched' );
-
-    if ( this.pendingLocks.length === 0 && this.callback ) {
-
-      this.launchBegan = true;
+    asyncLoader.readyToProceed( () => {
 
       window.phet.joist.launchSimulation = () => {
+        assert && assert( !this.launchComplete, 'should not have completed launching the sim yet' );
+        this.launchComplete = true;
 
         // once launchSimulation has been called, the wrapper is ready to receive messages because any listeners it
         // wants have been set up by now.
@@ -83,7 +55,7 @@ class SimLauncher {
         }
 
         // Instantiate the sim and show it.
-        this.callback();
+        callback();
       };
 
       // PhET-iO simulations support an initialization phase (before the sim launches)
@@ -104,35 +76,16 @@ class SimLauncher {
         // Wait for phet-io to finish adding listeners. It will direct the launch from there.
       }
       else {
-        this.launchComplete = true;
         window.phet.joist.launchSimulation();
       }
-    }
-  }
+    } );
 
-  /**
-   * Creates a lock, which returns a callback that needs to be run before the launch sequence will continue to the
-   * next phase.
-   * @public
-   *
-   * @param {*} object
-   * @returns {function}
-   */
-  createLock( object ) {
-    assert && assert( !this.launchBegan, 'Cannot create more locks after launch began' );
-    this.pendingLocks.push( object );
-    return () => {
-      assert && assert( this.pendingLocks.indexOf( object ) >= 0, 'invalid lock' );
-      arrayRemove( this.pendingLocks, object );
-      this.proceedIfReady();
-    };
+    // Signify that the simLauncher was called, see https://github.com/phetsims/joist/issues/142
+    window.phet.joist.launchCalled = true;
   }
 }
 
 const simLauncher = new SimLauncher();
-
-// Dynamic imports must be started after simLauncher is ready to process locks
-initializeDynamicImports( simLauncher );
 
 joist.register( 'simLauncher', simLauncher );
 
