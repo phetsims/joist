f08abbb088afde8b581412293acbd4076922aec9
Jesse <jesse.greenberg@colorado.edu>
2021-02-01 12:49:25 -0500

    HomeScreenButtons fire on up instead of down, see phetsims/joist#658

--- js/HomeScreenButton.js
+++ js/HomeScreenButton.js
@@ -180,7 +180,6 @@ class HomeScreenButton extends VBox {
     };
 
     const fireListener = new FireListener( {
-      fireOnDown: true, // to match prior behavior
       fire: buttonDown,
       tandem: options.tandem.createTandem( 'inputListener' )
     } );
