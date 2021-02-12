dca767dbeed6c4ceb1a7c7f2cc8bd0b467d59fd9
Jesse <jesse.greenberg@colorado.edu>
2021-02-12 18:46:25 -0500

    focus title after the current ScreenView is visible, see phetsims/ratio-and-proportion#321

--- js/Sim.js
+++ js/Sim.js
@@ -770,9 +770,6 @@ class Sim {
         // screen.isActiveProperty should change only while the screen is invisible, https://github.com/phetsims/joist/issues/418
         if ( visible ) {
           screen.activeProperty.set( visible );
-
-          // When a new screen is made visible focus should start at the top of the screen.
-          screen.view.focusTitle();
         }
         screen.view.setVisible( visible );
         if ( !visible ) {
@@ -784,6 +781,9 @@ class Sim {
         }
       } );
       this.updateBackground();
+
+      // When a new screen is made visible focus should start at the top of the screen.
+      currentScreen.view.focusTitle();
 
       if ( !this.isSettingPhetioStateProperty.value ) {
 
