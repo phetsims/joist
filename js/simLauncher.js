e78f18c217084ce9500fb9c27d3b4883867ac9a1
samreid <reids@colorado.edu>
2021-02-26 11:31:12 -0700

    Opt out of indent rule for files with import(), see https://github.com/phetsims/phet-info/issues/150

--- js/simLauncher.js
+++ js/simLauncher.js
@@ -1,4 +1,5 @@
 // Copyright 2013-2021, University of Colorado Boulder
+/* eslint indent: 0 */ // Because ESLint doesn't support import() at the time of this writing
 /**
  * Singleton which launches a PhET Simulation, after using PHET_CORE/asyncLoader to make sure resources such as images,
  * sounds, or dynamic modules have finished loading.
