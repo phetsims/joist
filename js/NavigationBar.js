a98cd7442fcd65ba432ae9208ad66ded9a4ba446
Jonathan Olson <jonathan.olson@colorado.edu>
2021-05-13 09:32:33 -0600

    Adding initialScreen/homeScreen, see https://github.com/phetsims/joist/issues/714

--- js/NavigationBar.js
+++ js/NavigationBar.js
@@ -136,7 +136,8 @@ define( function( require ) {
             sim.showHomeScreenProperty.value = true;
           }
         } );
-      this.barContents.addChild( this.homeButton );
+      // Add the home button, but only if it isn't turned off with ?homeScreen=false
+      phet.chipper.queryParameters.homeScreen && this.barContents.addChild( this.homeButton );
 
       /*
        * Allocate remaining horizontal space equally for screen buttons, assuming they will be centered in the navbar.
