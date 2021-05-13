bf7d8e65ce1cee1dba7d6e55d051c8f317ac40f7
Jonathan Olson <jonathan.olson@colorado.edu>
2021-05-13 08:39:23 -0600

    Removing unneeded errors for https://github.com/phetsims/joist/issues/714

--- js/Sim.js
+++ js/Sim.js
@@ -265,20 +265,6 @@ define( require => {
       phetioPlayback: true
     } );
 
-    if ( screens.length === 1 ) {
-
-      // Problems related to query parameters throw errors instead of assertions (so they are not stripped out)
-      if ( QueryStringMachine.containsKey( 'homeScreen' ) ) {
-        throw new Error( 'homeScreen query parameter not supported for single-screen sims' );
-      }
-      if ( QueryStringMachine.containsKey( 'initialScreen' ) ) {
-        throw new Error( 'initialScreen query parameter not supported for single-screen sims' );
-      }
-      if ( QueryStringMachine.containsKey( 'screens' ) ) {
-        throw new Error( 'screens query parameter not supported for single-screen sims' );
-      }
-    }
-
     let initialScreen = phet.chipper.queryParameters.initialScreen;
     const homeScreen = phet.chipper.queryParameters.homeScreen;
 
