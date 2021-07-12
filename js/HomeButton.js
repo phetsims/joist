de930fd989d55404fa7aad0f836cbebeda874cca
samreid <reids@colorado.edu>
2021-07-12 15:15:16 -0600

    Convert home icon to font awesome 5, see https://github.com/phetsims/sherpa/issues/83

--- js/HomeButton.js
+++ js/HomeButton.js
@@ -11,9 +11,10 @@ import Shape from '../../kite/js/Shape.js';
 import merge from '../../phet-core/js/merge.js';
 import FocusHighlightPath from '../../scenery/js/accessibility/FocusHighlightPath.js';
 import Node from '../../scenery/js/nodes/Node.js';
+import Path from '../../scenery/js/nodes/Path.js';
 import Rectangle from '../../scenery/js/nodes/Rectangle.js';
+import homeSolidShape from '../../sherpa/js/fontawesome-5/homeSolidShape.js';
 import ButtonInteractionState from '../../sun/js/buttons/ButtonInteractionState.js';
-import FontAwesomeNode from '../../sun/js/FontAwesomeNode.js';
 import joist from './joist.js';
 import JoistButton from './JoistButton.js';
 import joistStrings from './joistStrings.js';
@@ -44,7 +45,7 @@ class HomeButton extends JoistButton {
 
     assert && assert( !options.innerContent, 'HomeButton sets its own innerContent' );
 
-    const homeIcon = new FontAwesomeNode( 'home' );
+    const homeIcon = new Path( homeSolidShape );
 
     // scale so that the icon is slightly taller than screen button icons, value determined empirically, see joist#127
     homeIcon.setScaleMagnitude( 0.48 * navBarHeight / homeIcon.height );
