8e6d0d4378d9c25396a698dd4f1d18e78a3f87e6
Sam Reid <reids@colorado.edu>
2021-11-18 17:44:15 -0700

    Run grunt update, see https://github.com/phetsims/chipper/issues/1148

---
+++ js/joistStrings.ts
@@ -0,0 +1,255 @@
+// Copyright 2020-2021, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+/* eslint-disable */
+import getStringModule from '../../chipper/js/getStringModule.js';
+import joist from './joist.js';
+
+type StringsType = {
+  'joist': {
+    'title': string
+  },
+  'done': string,
+  'menuItem': {
+    'options': string,
+    'about': string,
+    'outputLog': string,
+    'phetWebsite': string,
+    'reportAProblem': string,
+    'screenshot': string,
+    'fullscreen': string,
+    'getUpdate': string,
+    'enhancedSound': string
+  },
+  'title': {
+    'settings': string
+  },
+  'showPointers': string,
+  'termsPrivacyAndLicensing': string,
+  'credits': {
+    'title': string,
+    'leadDesign': string,
+    'softwareDevelopment': string,
+    'team': string,
+    'contributors': string,
+    'qualityAssurance': string,
+    'graphicArts': string,
+    'soundDesign': string,
+    'translation': string,
+    'thanks': string
+  },
+  'options': {
+    'title': string
+  },
+  'versionPattern': string,
+  'updates': {
+    'upToDate': string,
+    'outOfDate': string,
+    'checking': string,
+    'offline': string,
+    'newVersionAvailable': string,
+    'yourCurrentVersion': string,
+    'getUpdate': string,
+    'noThanks': string
+  },
+  'translation': {
+    'credits': {
+      'link': string
+    }
+  },
+  'thirdParty': {
+    'credits': {
+      'link': string
+    }
+  },
+  'keyboardShortcuts': {
+    'title': string,
+    'toGetStarted': string
+  },
+  'simTitleWithScreenNamePattern': string,
+  'projectorMode': string,
+  'queryParametersWarningDialog': {
+    'invalidQueryParameters': string,
+    'oneOrMoreQueryParameters': string,
+    'theSimulationWillStart': string
+  },
+  'ieErrorPage': {
+    'platformError': string,
+    'ieIsNotSupported': string,
+    'useDifferentBrowser': string
+  },
+  'preferences': {
+    'title': string,
+    'tabs': {
+      'general': {
+        'title': string,
+        'accessibilityIntro': string,
+        'moreAccessibility': string,
+        'simulationSpecificSettings': string
+      },
+      'visual': {
+        'title': string,
+        'interactiveHighlights': string,
+        'interactiveHighlightsDescription': string
+      },
+      'audio': {
+        'title': string,
+        'sounds': {
+          'title': string,
+          'extraSounds': {
+            'title': string,
+            'description': string
+          },
+          'description': string
+        },
+        'audioFeatures': {
+          'title': string
+        }
+      },
+      'input': {
+        'title': string,
+        'gestureControls': {
+          'title': string,
+          'description': string
+        }
+      }
+    }
+  },
+  'a11y': {
+    'keyboardHelp': {
+      'keyboardShortcuts': string,
+      'tabToGetStarted': string
+    },
+    'playArea': string,
+    'controlArea': string,
+    'simScreens': string,
+    'simScreen': string,
+    'screenNamePattern': string,
+    'screenSimPattern': string,
+    'home': string,
+    'homeScreenDescription': string,
+    'homeScreenDescriptionPattern': string,
+    'simResources': string,
+    'soundToggle': {
+      'alert': {
+        'simSoundOn': string,
+        'simSoundOff': string
+      },
+      'label': string
+    },
+    'checkOutShortcuts': string,
+    'phetMenu': string,
+    'preferences': {
+      'tabs': {
+        'labelledDescriptionPattern': string,
+        'tabResponsePattern': string,
+        'visual': {
+          'interactiveHighlights': {
+            'enabledAlert': string,
+            'disabledAlert': string
+          }
+        },
+        'audio': {
+          'sounds': {
+            'soundsOn': string,
+            'soundsOff': string,
+            'extraSounds': {
+              'extraSoundsOn': string,
+              'extraSoundsOff': string
+            }
+          },
+          'voicing': {
+            'title': string,
+            'description': string,
+            'voicingOn': string,
+            'voicingOff': string,
+            'toolbar': {
+              'title': string,
+              'toolbarAdded': string,
+              'toolbarRemoved': string
+            },
+            'simVoicingOptions': {
+              'title': string,
+              'description': string,
+              'objectDetails': {
+                'label': string,
+                'enabledAlert': string,
+                'disabledAlert': string
+              },
+              'contextChanges': {
+                'label': string,
+                'enabledAlert': string,
+                'disabledAlert': string
+              },
+              'helpfulHints': {
+                'label': string,
+                'enabledAlert': string,
+                'disabledAlert': string
+              }
+            },
+            'customizeVoice': {
+              'title': string,
+              'expandedAlert': string,
+              'collapsedAlert': string,
+              'variablesPattern': string,
+              'writtenVariablesPattern': string,
+              'voice': {
+                'title': string,
+                'noVoicesAvailable': string
+              },
+              'rate': {
+                'title': string,
+                'labelString': string,
+                'rangeDescriptions': {
+                  'low': string,
+                  'normal': string,
+                  'aboveNormal': string,
+                  'high': string,
+                  'voiceRateNormal': string
+                }
+              },
+              'pitch': {
+                'title': string
+              }
+            }
+          }
+        },
+        'input': {
+          'gestureControl': {
+            'enabledAlert': string,
+            'disabledAlert': string
+          }
+        }
+      }
+    },
+    'toolbar': {
+      'title': string,
+      'openToolbar': string,
+      'closeToolbar': string,
+      'hideToolbar': string,
+      'showToolbar': string,
+      'toolbarShown': string,
+      'toolbarHidden': string,
+      'voicing': {
+        'title': string,
+        'quickInfo': string,
+        'playOverviewLabel': string,
+        'playDetailsLabel': string,
+        'playHintLabel': string,
+        'overviewLabel': string,
+        'detailsLabel': string,
+        'hintLabel': string,
+        'simVoicingOnAlert': string,
+        'simVoicingOffAlert': string
+      }
+    }
+  }
+};
+
+const joistStrings = getStringModule( 'JOIST' ) as StringsType;
+
+joist.register( 'joistStrings', joistStrings );
+
+export default joistStrings;
