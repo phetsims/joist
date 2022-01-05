26233fd5cd39f1b0fefab5dd87dc85881e9cac0e
phet-dev <phetadmin@gmail.com>
2022-01-05 03:21:44 -0700

    update copyright dates from daily grunt work

--- js/KeyboardHelpDialog.js
+++ js/KeyboardHelpDialog.js
@@ -1,4 +1,4 @@
-// Copyright 2016-2021, University of Colorado Boulder
+// Copyright 2016-2022, University of Colorado Boulder
 
 /**
  * Shows a Dialog with content describing keyboard interactions. Brought up by a button
