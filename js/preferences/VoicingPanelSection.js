a2518a612a7a1118f663de2c3bae03c69c91e10d
phet-dev <phetadmin@gmail.com>
2022-01-26 03:32:00 -0700

    update copyright dates from daily grunt work

--- js/preferences/VoicingPanelSection.js
+++ js/preferences/VoicingPanelSection.js
@@ -1,4 +1,4 @@
-// Copyright 2021, University of Colorado Boulder
+// Copyright 2021-2022, University of Colorado Boulder
 
 /**
  * The section of PreferencesDialog content in the "Audio" panel related to voicing.
