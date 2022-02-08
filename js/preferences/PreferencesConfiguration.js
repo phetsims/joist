cc3ae41fca51ca50bab7bd5449aa4e77fad06290
phet-dev <phetadmin@gmail.com>
2022-02-08 03:24:23 -0700

    update copyright dates from daily grunt work

--- js/preferences/PreferencesConfiguration.js
+++ js/preferences/PreferencesConfiguration.js
@@ -1,4 +1,4 @@
-// Copyright 2021, University of Colorado Boulder
+// Copyright 2021-2022, University of Colorado Boulder
 
 /**
  * Configures the Preferences for a simulation, signifying which features are enabled in the simulation and will
