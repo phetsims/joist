bee67407822a3b6d90500f4628ea033cd522eb38
pixelzoom <cmalley@pixelzoom.com>
2022-03-28 10:27:30 -0600

    convert to TS

--- js/ScreenIcon.ts
+++ js/ScreenIcon.ts
@@ -1,35 +1,46 @@
-// Copyright 2015-2021, University of Colorado Boulder
+// Copyright 2015-2022, University of Colorado Boulder
 
-// @ts-nocheck
 /**
- * An icon suitable for one of the screen-selection buttons on the home screen or navigation bar.
+ * ScreenIcon is an icon that is suitable for one of the screen-selection buttons on the home screen or navigation bar.
  * By default, the size is optimized for the home screen.
  * To optimize for the navigation bar, use options.size: Screen.MINIMUM_NAVBAR_ICON_SIZE
  *
  * @author Chris Malley (PixelZoom, Inc.)
  */
 
-import merge from '../../phet-core/js/merge.js';
-import { Node } from '../../scenery/js/imports.js';
-import { Rectangle } from '../../scenery/js/imports.js';
+import optionize from '../../phet-core/js/optionize.js';
+import { IColor, Node, NodeOptions, Rectangle } from '../../scenery/js/imports.js';
 import joist from './joist.js';
 import Screen from './Screen.js';
+import Dimension2 from '../../dot/js/Dimension2.js';
+
+type SelfOptions = {
+  size?: Dimension2; // size of the background
+  maxIconWidthProportion?: number; // max proportion of the background width occupied by iconNode, (0,1]
+  maxIconHeightProportion?: number; // max proportion of the background height occupied by iconNode, (0,1]
+  fill?: IColor; // {Color|string} background fill
+  stroke?: IColor; // {Color|string} background stroke
+};
+
+export type ScreenIconOptions = SelfOptions & NodeOptions;
 
 class ScreenIcon extends Node {
 
   /**
-   * @param {Node} iconNode
-   * @param {Object} [options]
+   * @param iconNode
+   * @param providedOptions
    */
-  constructor( iconNode, options ) {
+  constructor( iconNode: Node, providedOptions?: ScreenIconOptions ) {
 
-    options = merge( {
+    const options = optionize<ScreenIconOptions, SelfOptions, NodeOptions>( {
+
+      // SelfOptions
       size: Screen.MINIMUM_HOME_SCREEN_ICON_SIZE, // {Dimension2} size of the background
       maxIconWidthProportion: 0.85, // max proportion of the background width occupied by iconNode, (0,1]
       maxIconHeightProportion: 0.85, // max proportion of the background height occupied by iconNode, (0,1]
       fill: 'white', // {Color|string} background fill
       stroke: null // {Color|string} background stroke
-    }, options );
+    }, providedOptions );
 
     assert && assert( options.maxIconWidthProportion > 0 && options.maxIconWidthProportion <= 1 );
     assert && assert( options.maxIconHeightProportion > 0 && options.maxIconHeightProportion <= 1 );
