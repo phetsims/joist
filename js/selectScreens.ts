f74eeb5266ea9c9ca2767f8198d58dbf489107aa
phet-dev <phetadmin@gmail.com>
2022-04-16 03:36:48 -0600

    update copyright dates from daily grunt work

--- js/selectScreens.ts
+++ js/selectScreens.ts
@@ -1,4 +1,4 @@
-// Copyright 2020-2021, University of Colorado Boulder
+// Copyright 2020-2022, University of Colorado Boulder
 import joist from './joist.js';
 import HomeScreen from './HomeScreen.js';
 import Screen from './Screen.js';
