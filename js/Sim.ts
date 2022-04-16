11a792cd0149cf986c66075a2628cddcea804582
Sam Reid <reids@colorado.edu>
2022-04-16 09:31:44 -0600

    Remove unused expressions, see https://github.com/phetsims/joist/issues/795 and https://github.com/phetsims/chipper/issues/954

--- js/Sim.ts
+++ js/Sim.ts
@@ -70,11 +70,6 @@ import { CreditsData } from './CreditsNode.js';
 const PROGRESS_BAR_WIDTH = 273;
 const SUPPORTS_GESTURE_DESCRIPTION = platform.android || platform.mobileSafari;
 
-// strings needed for the IE warning dialog
-joistStrings.ieErrorPage.platformError;
-joistStrings.ieErrorPage.ieIsNotSupported;
-joistStrings.ieErrorPage.useDifferentBrowser;
-
 // globals
 phet.joist.elapsedTime = 0; // in milliseconds, use this in Tween.start for replicable playbacks
 
