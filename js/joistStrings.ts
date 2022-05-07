c4e16a3879c721f2d204b8f8389723ebe1c24495
Sam Reid <reids@colorado.edu>
2022-05-07 09:37:13 -0600

    Use semicolons to terminate fields in type StringType, see https://github.com/phetsims/chipper/issues/1208

--- js/joistStrings.ts
+++ js/joistStrings.ts
@@ -9,235 +9,235 @@ import joist from './joist.js';
 
 type StringsType = {
   'joist': {
-    'title': string
-  },
-  'done': string,
+    'title': string;
+  };
+  'done': string;
   'menuItem': {
-    'options': string,
-    'about': string,
-    'outputLog': string,
-    'phetWebsite': string,
-    'reportAProblem': string,
-    'screenshot': string,
-    'fullscreen': string,
-    'getUpdate': string,
-    'enhancedSound': string
-  },
+    'options': string;
+    'about': string;
+    'outputLog': string;
+    'phetWebsite': string;
+    'reportAProblem': string;
+    'screenshot': string;
+    'fullscreen': string;
+    'getUpdate': string;
+    'enhancedSound': string;
+  };
   'title': {
-    'settings': string
-  },
-  'showPointers': string,
-  'termsPrivacyAndLicensing': string,
+    'settings': string;
+  };
+  'showPointers': string;
+  'termsPrivacyAndLicensing': string;
   'credits': {
-    'title': string,
-    'leadDesign': string,
-    'softwareDevelopment': string,
-    'team': string,
-    'contributors': string,
-    'qualityAssurance': string,
-    'graphicArts': string,
-    'soundDesign': string,
-    'translation': string,
-    'thanks': string
-  },
+    'title': string;
+    'leadDesign': string;
+    'softwareDevelopment': string;
+    'team': string;
+    'contributors': string;
+    'qualityAssurance': string;
+    'graphicArts': string;
+    'soundDesign': string;
+    'translation': string;
+    'thanks': string;
+  };
   'options': {
-    'title': string
-  },
-  'versionPattern': string,
+    'title': string;
+  };
+  'versionPattern': string;
   'updates': {
-    'upToDate': string,
-    'outOfDate': string,
-    'checking': string,
-    'offline': string,
-    'newVersionAvailable': string,
-    'yourCurrentVersion': string,
-    'getUpdate': string,
-    'noThanks': string
-  },
+    'upToDate': string;
+    'outOfDate': string;
+    'checking': string;
+    'offline': string;
+    'newVersionAvailable': string;
+    'yourCurrentVersion': string;
+    'getUpdate': string;
+    'noThanks': string;
+  };
   'translation': {
     'credits': {
-      'link': string
+      'link': string;
     }
-  },
+  };
   'thirdParty': {
     'credits': {
-      'link': string
+      'link': string;
     }
-  },
+  };
   'keyboardShortcuts': {
-    'title': string,
-    'toGetStarted': string
-  },
-  'simTitleWithScreenNamePattern': string,
-  'projectorMode': string,
+    'title': string;
+    'toGetStarted': string;
+  };
+  'simTitleWithScreenNamePattern': string;
+  'projectorMode': string;
   'queryParametersWarningDialog': {
-    'invalidQueryParameters': string,
-    'oneOrMoreQueryParameters': string,
-    'theSimulationWillStart': string
-  },
+    'invalidQueryParameters': string;
+    'oneOrMoreQueryParameters': string;
+    'theSimulationWillStart': string;
+  };
   'ieErrorPage': {
-    'platformError': string,
-    'ieIsNotSupported': string,
-    'useDifferentBrowser': string
-  },
+    'platformError': string;
+    'ieIsNotSupported': string;
+    'useDifferentBrowser': string;
+  };
   'preferences': {
-    'title': string,
+    'title': string;
     'tabs': {
       'general': {
-        'title': string,
-        'accessibilityIntro': string,
-        'moreAccessibility': string
-      },
+        'title': string;
+        'accessibilityIntro': string;
+        'moreAccessibility': string;
+      };
       'visual': {
-        'title': string,
-        'interactiveHighlights': string,
-        'interactiveHighlightsDescription': string
-      },
+        'title': string;
+        'interactiveHighlights': string;
+        'interactiveHighlightsDescription': string;
+      };
       'audio': {
-        'title': string,
+        'title': string;
         'sounds': {
-          'title': string,
+          'title': string;
           'extraSounds': {
-            'title': string,
-            'description': string
-          },
-          'description': string
-        },
+            'title': string;
+            'description': string;
+          };
+          'description': string;
+        };
         'audioFeatures': {
-          'title': string
+          'title': string;
         }
       }
     }
-  },
+  };
   'a11y': {
     'keyboardHelp': {
-      'keyboardShortcuts': string,
-      'tabToGetStarted': string
-    },
-    'playArea': string,
-    'controlArea': string,
-    'simScreens': string,
-    'simScreen': string,
-    'screenNamePattern': string,
-    'screenSimPattern': string,
-    'home': string,
-    'homeScreenDescription': string,
-    'homeScreenDescriptionPattern': string,
-    'homeScreenHint': string,
-    'homeScreenIntroPattern': string,
-    'homeScreenButtonDetailsPattern': string,
-    'simResources': string,
+      'keyboardShortcuts': string;
+      'tabToGetStarted': string;
+    };
+    'playArea': string;
+    'controlArea': string;
+    'simScreens': string;
+    'simScreen': string;
+    'screenNamePattern': string;
+    'screenSimPattern': string;
+    'home': string;
+    'homeScreenDescription': string;
+    'homeScreenDescriptionPattern': string;
+    'homeScreenHint': string;
+    'homeScreenIntroPattern': string;
+    'homeScreenButtonDetailsPattern': string;
+    'simResources': string;
     'soundToggle': {
       'alert': {
-        'simSoundOn': string,
-        'simSoundOff': string
-      },
-      'label': string
-    },
-    'checkOutShortcuts': string,
-    'phetMenu': string,
+        'simSoundOn': string;
+        'simSoundOff': string;
+      };
+      'label': string;
+    };
+    'checkOutShortcuts': string;
+    'phetMenu': string;
     'preferences': {
       'tabs': {
-        'labelledDescriptionPattern': string,
-        'tabResponsePattern': string,
+        'labelledDescriptionPattern': string;
+        'tabResponsePattern': string;
         'visual': {
           'interactiveHighlights': {
-            'enabledAlert': string,
-            'disabledAlert': string
+            'enabledAlert': string;
+            'disabledAlert': string;
           }
-        },
+        };
         'audio': {
           'sounds': {
-            'soundsOn': string,
-            'soundsOff': string,
+            'soundsOn': string;
+            'soundsOff': string;
             'extraSounds': {
-              'extraSoundsOn': string,
-              'extraSoundsOff': string
+              'extraSoundsOn': string;
+              'extraSoundsOff': string;
             }
-          },
+          };
           'voicing': {
-            'title': string,
-            'description': string,
-            'voicingOn': string,
-            'voicingOff': string,
+            'title': string;
+            'description': string;
+            'voicingOn': string;
+            'voicingOff': string;
             'toolbar': {
-              'title': string,
-              'toolbarAdded': string,
-              'toolbarRemoved': string
-            },
+              'title': string;
+              'toolbarAdded': string;
+              'toolbarRemoved': string;
+            };
             'simVoicingOptions': {
-              'title': string,
-              'description': string,
+              'title': string;
+              'description': string;
               'objectDetails': {
-                'label': string,
-                'enabledAlert': string,
-                'disabledAlert': string
-              },
+                'label': string;
+                'enabledAlert': string;
+                'disabledAlert': string;
+              };
               'contextChanges': {
-                'label': string,
-                'enabledAlert': string,
-                'disabledAlert': string
-              },
+                'label': string;
+                'enabledAlert': string;
+                'disabledAlert': string;
+              };
               'helpfulHints': {
-                'label': string,
-                'enabledAlert': string,
-                'disabledAlert': string
+                'label': string;
+                'enabledAlert': string;
+                'disabledAlert': string;
               }
-            },
+            };
             'customizeVoice': {
-              'title': string,
-              'expandedAlert': string,
-              'collapsedAlert': string,
-              'variablesPattern': string,
-              'writtenVariablesPattern': string,
+              'title': string;
+              'expandedAlert': string;
+              'collapsedAlert': string;
+              'variablesPattern': string;
+              'writtenVariablesPattern': string;
               'voice': {
-                'title': string,
-                'noVoicesAvailable': string
-              },
+                'title': string;
+                'noVoicesAvailable': string;
+              };
               'rate': {
-                'title': string,
-                'labelString': string,
+                'title': string;
+                'labelString': string;
                 'rangeDescriptions': {
-                  'low': string,
-                  'normal': string,
-                  'aboveNormal': string,
-                  'high': string,
-                  'voiceRateNormal': string
+                  'low': string;
+                  'normal': string;
+                  'aboveNormal': string;
+                  'high': string;
+                  'voiceRateNormal': string;
                 }
-              },
+              };
               'pitch': {
-                'title': string
+                'title': string;
               }
             }
           }
-        },
+        };
         'input': {
           'gestureControl': {
-            'enabledAlert': string,
-            'disabledAlert': string
+            'enabledAlert': string;
+            'disabledAlert': string;
           }
         }
       }
-    },
+    };
     'toolbar': {
-      'title': string,
-      'openToolbar': string,
-      'closeToolbar': string,
-      'hideToolbar': string,
-      'showToolbar': string,
-      'toolbarShown': string,
-      'toolbarHidden': string,
+      'title': string;
+      'openToolbar': string;
+      'closeToolbar': string;
+      'hideToolbar': string;
+      'showToolbar': string;
+      'toolbarShown': string;
+      'toolbarHidden': string;
       'voicing': {
-        'title': string,
-        'quickInfo': string,
-        'playOverviewLabel': string,
-        'playDetailsLabel': string,
-        'playHintLabel': string,
-        'overviewLabel': string,
-        'detailsLabel': string,
-        'hintLabel': string,
-        'simVoicingOnAlert': string,
-        'simVoicingOffAlert': string
+        'title': string;
+        'quickInfo': string;
+        'playOverviewLabel': string;
+        'playDetailsLabel': string;
+        'playHintLabel': string;
+        'overviewLabel': string;
+        'detailsLabel': string;
+        'hintLabel': string;
+        'simVoicingOnAlert': string;
+        'simVoicingOffAlert': string;
       }
     }
   }
