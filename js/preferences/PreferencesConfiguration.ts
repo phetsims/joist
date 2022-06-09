c31d8bfa6ef6acb5178fa1b398ffb62aad8a28dd
Jesse <jesse.greenberg@colorado.edu>
2022-06-09 16:33:30 -0400

    TypeScript conversion of PreferencesConfiguration.ts, see https://github.com/phetsims/sun/issues/763

--- js/preferences/PreferencesConfiguration.ts
+++ js/preferences/PreferencesConfiguration.ts
@@ -8,76 +8,97 @@
  * @author Jesse Greenberg (PhET Interactive Simulations)
  */
 
-import merge from '../../../phet-core/js/merge.js';
+import optionize from '../../../phet-core/js/optionize.js';
+import { Node } from '../../../scenery/js/imports.js';
 import SpeechSynthesisAnnouncer from '../../../utterance-queue/js/SpeechSynthesisAnnouncer.js';
 import joist from '../joist.js';
 
+type GeneralOptions = {
+
+  // any Node you would like to put under the "General" tab with sim-specific controls
+  simControls?: Node | null;
+}
+
+type VisualOptions = {
+
+  // whether or not the sim supports the "Interactive Highlights" feature, and checkbox to enable in the
+  // Preferences Dialog
+  supportsInteractiveHighlights?: boolean;
+}
+
+type AudioOptions = {
+
+  // The entry point for Voicing, and if true the sim will support Voicing and Voicing options in Preferences.
+  // The feature is only available on platforms where SpeechSynthesis is supported. For now, it is only available
+  // when running with english locales, accessibility strings are not made available for translation yet.
+  supportsVoicing?: boolean;
+
+  // Whether or not to include checkboxes related to sound and enhanced sound. supportsEnhancedSound can only be
+  // included if supportsSound is also true.
+  supportsSound?: boolean;
+  supportsEnhancedSound?: boolean;
+}
+
+type InputOptions = {
+
+  // Whether or not to include "gesture" controls
+  supportsGestureControl?: boolean;
+}
+
+export type PreferencesConfigurationOptions = {
+
+  // configuration for controls in the "General" tab of the PreferencesDialog
+  generalOptions?: GeneralOptions;
+
+  // configuration for controls in the "Visual" tab of the PreferencesDialog
+  visualOptions?: VisualOptions;
+
+  // configuration for controls in the "Audio" tab of the PreferencesDialog
+  audioOptions?: AudioOptions;
+
+  // configuration for controls in the "Input" tab of the PreferencesDialog
+  inputOptions?: InputOptions;
+}
+
 class PreferencesConfiguration {
 
-  /**
-   * @param {Object} [options]
-   */
-  constructor( options ) {
+  public readonly generalOptions: GeneralOptions;
+  public readonly visualOptions: VisualOptions;
+  public readonly audioOptions: AudioOptions;
+  public readonly inputOptions: InputOptions;
+
+  constructor( providedOptions?: PreferencesConfigurationOptions ) {
 
     // initialize-globals uses package.json to determine defaults for features enabled by the sim and those defaults
-    // can be overwritten by query parameter.  So it contains an accurate representation of which features
-    // are requested.
+    // can be overwritten by query parameter.  So phet.chipper.queryParameters contains an accurate representation of
+    // which features are required.
     const phetFeatures = phet.chipper.queryParameters;
 
     // For now, the Voicing feature is only available when we are running in the english locale, accessibility
     // strings are not made available for translation.
     const simLocale = phet.chipper.locale || 'en';
 
-    options = merge( {
-
-      // configuration for controls in the "General" tab of the PreferencesDialog
+    const options = optionize<PreferencesConfigurationOptions>()( {
       generalOptions: {
-
-        // {Node|null} - any Node you would like to put under the "General" tab with sim-specific controls
         simControls: null
       },
-
-      // configuration for controls in the "Visual" tab of the PreferencesDialog
       visualOptions: {
-
-        // {boolean} whether or not the sim supports the "interactive highlights" feature, and checkbox to enable
-        // in the PreferencesDialog
         supportsInteractiveHighlights: phetFeatures.supportsInteractiveHighlights
       },
-
-      // configuration for controls in the "Audio" tab of the PreferencesDialog
       audioOptions: {
-
-        // The entry point for Voicing, and if true the sim will support Voicing and Voicing options in Preferences.
-        // The feature is only available on platforms where SpeechSynthesis is supported. For now, it is only available
-        // when running with english locales, accessibility strings are not made available for translation yet.
         supportsVoicing: phetFeatures.supportsVoicing && SpeechSynthesisAnnouncer.isSpeechSynthesisSupported() && simLocale === 'en',
-
-        // {boolean} - Whether or not to include checkboxes related to sound and enhanced sound. supportsEnhancedSound
-        // can only be included if supportsSound is also true.
-        // NOTE: When initialize-globals uses package.json to control sound features the packageJSON check
-        // here can be removed
         supportsSound: phetFeatures.supportsSound,
         supportsEnhancedSound: phetFeatures.supportsEnhancedSound
       },
-
-      // configuration for controls in the "Input" tab of the PreferencesDialog
       inputOptions: {
-
-        // {boolean} - Whether or not to include "gesture" controls
         supportsGestureControl: phetFeatures.supportsGestureControl
       }
-    }, options );
+    }, providedOptions );
 
-    assert && assert( options.generalOptions, 'generalOptions must be defined' );
-    assert && assert( options.visualOptions, 'visualOptions must be defined' );
-    assert && assert( options.audioOptions, 'audioOptions must be defined' );
-    assert && assert( options.inputOptions, 'inputOptions must be defined' );
     if ( options.audioOptions.supportsEnhancedSound ) {
       assert && assert( options.audioOptions.supportsSound, 'supportsSound must be true to also support enhancedSound' );
     }
 
-    // @public (read-only)
     this.generalOptions = options.generalOptions;
     this.visualOptions = options.visualOptions;
     this.audioOptions = options.audioOptions;
