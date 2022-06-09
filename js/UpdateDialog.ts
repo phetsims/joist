5a762f5f49260e8e6214640804d76d43056e378c
phet-dev <phetadmin@gmail.com>
2022-06-09 03:40:48 -0600

    update copyright dates from daily grunt work

--- js/UpdateDialog.ts
+++ js/UpdateDialog.ts
@@ -1,4 +1,4 @@
-// Copyright 2015-2021, University of Colorado Boulder
+// Copyright 2015-2022, University of Colorado Boulder
 
 /**
  * UpdateDialog is a fixed-size dialog that displays the current update status.
