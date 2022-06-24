b3353c6529e7aed226d7496f76c6129a1cba5ec1
phet-dev <phetadmin@gmail.com>
2022-06-24 03:52:34 -0600

    update copyright dates from daily grunt work

--- js/ProjectorModeCheckbox.js
+++ js/ProjectorModeCheckbox.js
@@ -1,4 +1,4 @@
-// Copyright 2018-2021, University of Colorado Boulder
+// Copyright 2018-2022, University of Colorado Boulder
 
 /**
  * ProjectorModeCheckbox is a checkbox used to switch between 'default' and 'projector' color profiles.
