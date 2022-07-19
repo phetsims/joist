65c5c8e1a09d6faaa7860c69f341273bb856ec2e
Jesse <jesse.greenberg@colorado.edu>
2022-07-19 17:23:48 -0400

    PreferencesManager port to TypeScript, see https://github.com/phetsims/sun/issues/763

--- js/preferences/PreferencesManager.ts
+++ js/preferences/PreferencesManager.ts
@@ -1,7 +1,7 @@
 // Copyright 2021-2022, University of Colorado Boulder
 
 /**
- * A Class that manages Simulation features that enabled and disabled by user Preferences.
+ * A Class that manages Simulation features that are enabled and disabled by user Preferences.
  *
  * @author Jesse Greenberg
  */
@@ -13,36 +13,63 @@ import joist from '../joist.js';
 import PreferencesStorage from './PreferencesStorage.js';
 import soundManager from '../../../tambo/js/soundManager.js';
 import audioManager from '../audioManager.js';
+import PreferencesConfiguration, { AudioPreferencesOptions, GeneralPreferencesOptions, InputPreferencesOptions, VisualPreferencesOptions } from './PreferencesConfiguration.js';
+import IProperty from '../../../axon/js/IProperty.js';
+
+type GeneralModel = Required<GeneralPreferencesOptions>;
+
+type VisualModel = {
+
+  // Whether "Interactive Highlights" are enabled for the simulation. If enabled, focus highlights will appear around
+  // focusable components with 'over' events, and persist around the focused element even with mouse and touch
+  // interaction.
+  interactiveHighlightsEnabledProperty: IProperty<boolean>;
+} & Required<VisualPreferencesOptions>;
+
+type AudioModel = {
+  simSoundEnabledProperty: IProperty<boolean>;
+  soundEnabledProperty: IProperty<boolean>;
+  extraSoundEnabledProperty: IProperty<boolean>;
+
+  voicingEnabledProperty: IProperty<boolean>;
+  voicingMainWindowVoicingEnabledProperty: IProperty<boolean>;
+  voicingObjectResponsesEnabledProperty: IProperty<boolean>;
+  voicingContextResponsesEnabledProperty: IProperty<boolean>;
+  voicingHintResponsesEnabledProperty: IProperty<boolean>;
+  voicePitchProperty: IProperty<number>;
+  voiceRateProperty: IProperty<number>;
+  voiceProperty: IProperty<null | SpeechSynthesisVoice>;
+} & Required<AudioPreferencesOptions>;
+
+type InputModel = {
+
+  // Whether "Gesture Controls" are enabled for the simulation. If enabled, touch screen input will change to work
+  // like a screen reader. Horizontal swipes across the screen will move focus, double-taps will activate the
+  // selected item, and tap then hold will initiate drag and drop interactions. Note that enabling this will generally
+  // prevent all touch input from working as it does normally.
+  gestureControlsEnabledProperty: IProperty<boolean>;
+
+} & Required<InputPreferencesOptions>;
 
 class PreferencesManager {
 
-  /**
-   * @param {PreferencesConfiguration} preferencesConfiguration
-   */
-  constructor( preferencesConfiguration ) {
+  // Whether the Sim Toolbar is enabled, which gives quick access to various controls for the simulation or
+  // active screen.
+  public readonly toolbarEnabledProperty = new BooleanProperty( true );
 
-    assert && assert( preferencesConfiguration, 'PreferencesManager requires the sim to have PreferencesConfiguration' );
+  public readonly generalModel: GeneralModel;
+  public readonly visualModel: VisualModel;
+  public readonly audioModel: AudioModel;
+  public readonly inputModel: InputModel;
 
-
-    // @public {BooleanProperty} - Whether or not the Sim Toolbar is enabled, which gives quick access to various
-    // controls for the simulation or active screen.
-    this.toolbarEnabledProperty = new BooleanProperty( true );
-
-    // @public (read-only)
+  public constructor( preferencesConfiguration: PreferencesConfiguration ) {
     this.generalModel = preferencesConfiguration.generalOptions;
 
-    // @public (read-only)
     this.visualModel = {
       supportsInteractiveHighlights: preferencesConfiguration.visualOptions.supportsInteractiveHighlights,
-
-
-      // @public {BooleanProperty} - Whether or not "Interactive Highlights" are enabled for the simulation. If enabled,
-      // focus highlights will appear around focusable components with 'over' events, and persist around the focused
-      // element even with mouse and touch interaction.
       interactiveHighlightsEnabledProperty: new BooleanProperty( false )
     };
 
-    // @public (read-only)
     this.audioModel = {
       supportsVoicing: preferencesConfiguration.audioOptions.supportsVoicing,
       supportsSound: preferencesConfiguration.audioOptions.supportsSound,
@@ -62,14 +89,8 @@ class PreferencesManager {
       voiceProperty: voicingManager.voiceProperty
     };
 
-    // @public (read-only)
     this.inputModel = {
       supportsGestureControl: preferencesConfiguration.inputOptions.supportsGestureControl,
-
-      // @public {BooleanProperty} - Whether or not "Gesture Controls" are enabled for the simulation. If enabled,
-      // touch screen input will change to work like a screen reader. Horizontal swipes across the screen will move focus,
-      // double-taps will activate the selected item, and tap then hold will initiate drag and drop interactions.
-      // Note that enabling this will generally prevent all touch input from working as it does normally.
       gestureControlsEnabledProperty: new BooleanProperty( false )
     };
 
@@ -86,29 +107,17 @@ class PreferencesManager {
     PreferencesStorage.register( this.visualModel.interactiveHighlightsEnabledProperty, 'interactiveHighlightsEnabledProperty' );
   }
 
-  /**
-   * @public
-   * @returns {boolean}
-   */
-  supportsVisualPreferences() {
+  public supportsVisualPreferences(): boolean {
     return this.visualModel.supportsInteractiveHighlights;
   }
 
-  /**
-   * @public
-   * @returns {boolean}
-   */
-  supportsAudioPreferences() {
+  public supportsAudioPreferences(): boolean {
     return this.audioModel.supportsVoicing ||
            this.audioModel.supportsSound ||
            this.audioModel.supportsExtraSound;
   }
 
-  /**
-   * @public
-   * @returns {boolean}
-   */
-  supportsInputPreferences() {
+  public supportsInputPreferences(): boolean {
     return this.inputModel.supportsGestureControl;
   }
 }
