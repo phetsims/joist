f18a00515ff3c3bc7ca94962efc49807ca9d0fbd
Luisav1 <luisa.vargassuarez@ucalgary.ca>
2022-07-20 17:03:41 -0600

    Convert Frame to typescript. See https://github.com/phetsims/joist/issues/776.

--- js/Frame.ts
+++ js/Frame.ts
@@ -7,31 +7,36 @@
  */
 
 import Bounds2 from '../../dot/js/Bounds2.js';
-import merge from '../../phet-core/js/merge.js';
-import { LinearGradient, Node, Rectangle } from '../../scenery/js/imports.js';
+import optionize from '../../phet-core/js/optionize.js';
+import { LinearGradient, Node, Rectangle, NodeOptions } from '../../scenery/js/imports.js';
 import joist from './joist.js';
+
+type SelfOptions = {
+  xMargin1?: number;
+  yMargin1?: number;
+  cornerRadius?: number; // radius of the rounded corners on the background
+};
+export type FrameOptions = SelfOptions & NodeOptions;
 
 class Frame extends Node {
 
-  /**
-   * @param {Node} content
-   * @param {Object} [options]
-   */
-  constructor( content, options ) {
+  private readonly gradient: LinearGradient;
+  private readonly highlightRectangle: Rectangle;
+
+  public constructor( content: Node, providedOptions?: FrameOptions ) {
 
     // default options
-    options = merge( {
+    const options = optionize<FrameOptions, SelfOptions, NodeOptions>()( {
       xMargin1: 6,
       yMargin1: 6,
-      cornerRadius: 0 // radius of the rounded corners on the background
-    }, options );
+      cornerRadius: 0
+    }, providedOptions );
 
     super();
 
     const frameWidth = content.width + 2 * options.xMargin1;
     const frameHeight = content.height + 2 * options.yMargin1;
 
-    // @private
     this.gradient = new LinearGradient( 0, 0, frameWidth, 0 )
       .addColorStop( 0, '#fbff41' )
       .addColorStop( 118 / 800.0, '#fef98b' )
@@ -39,7 +44,6 @@ class Frame extends Node {
       .addColorStop( 616 / 800, '#fffccd' )
       .addColorStop( 1, '#fbff41' );
 
-    // @private
     const rectangle = new Rectangle( 0, 0, frameWidth, frameHeight, options.cornerRadius, options.cornerRadius, {
       stroke: this.gradient,
       lineWidth: 3,
@@ -51,7 +55,7 @@ class Frame extends Node {
     // Apply options after the layout is done, so that options that use the bounds will work properly.
     this.mutate( options );
 
-    // @private - highlight rectangle is always in the scene graph to make sure the node is positioned properly
+    // highlight rectangle is always in the scene graph to make sure the node is positioned properly
     // but only visible when highlighted
     const frameBounds = Bounds2.rect( rectangle.x, rectangle.y, frameWidth, frameHeight );
     this.highlightRectangle = Rectangle.bounds( frameBounds.dilated( 0.75 ), {
@@ -61,8 +65,7 @@ class Frame extends Node {
     this.addChild( this.highlightRectangle );
   }
 
-  // @public
-  setHighlighted( highlighted ) {
+  public setHighlighted( highlighted: boolean ): void {
     this.highlightRectangle.stroke = highlighted ? this.gradient : 'transparent';
   }
 }
