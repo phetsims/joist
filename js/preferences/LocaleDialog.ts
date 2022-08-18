1f0751ef058aae182c9934d0d94c6fbb9038946a
Jesse <jesse.greenberg@colorado.edu>
2022-08-18 15:15:43 -0400

    Demo using a carousel for langauges, see https://github.com/phetsims/joist/issues/814

--- js/preferences/LocaleDialog.ts
+++ js/preferences/LocaleDialog.ts
@@ -10,14 +10,13 @@
  */
 
 import joist from '../joist.js';
-import { GridBox, Text } from '../../../scenery/js/imports.js';
+import { FireListener, GridBox, Node, Text } from '../../../scenery/js/imports.js';
 import localeProperty from '../localeProperty.js';
 import localeInfoModule from '../../../chipper/js/data/localeInfoModule.js';
 import Dialog from '../../../sun/js/Dialog.js';
-import BooleanProperty from '../../../axon/js/BooleanProperty.js';
 import PreferencesDialog from './PreferencesDialog.js';
-import BooleanRectangularStickyToggleButton from '../../../sun/js/buttons/BooleanRectangularStickyToggleButton.js';
 import Tandem from '../../../tandem/js/Tandem.js';
+import Carousel from '../../../sun/js/Carousel.js';
 
 class LocaleDialog extends Dialog {
   public constructor() {
@@ -35,13 +34,8 @@ class LocaleDialog extends Dialog {
       }
     } );
 
-    const content = new GridBox( {
-      xMargin: 10,
-      yMargin: 3,
-      xAlign: 'center',
-      autoRows: 20,
-
-      children: localeProperty.validValues!.map( locale => {
+    function createInteractiveLocales(): Node[] {
+      return localeProperty.validValues!.map( locale => {
 
         // A prototype displaying TextPushButtons of matching width for each locale
         // @ts-ignore
@@ -57,44 +51,69 @@ class LocaleDialog extends Dialog {
 
         // A prototype displaying sticky toggle buttons for each language so it is clear which
         // one is selected.
-        const toggleButtonProperty = new BooleanProperty( false );
-        toggleButtonProperty.link( ( isPressed: boolean ) => {
-          if ( isPressed ) {
-            localeProperty.value = locale;
-          }
-        } );
-        localeProperty.link( pressedLocale => {
-          toggleButtonProperty.value = pressedLocale === locale;
-        } );
-
-        return new BooleanRectangularStickyToggleButton( toggleButtonProperty, {
-
-          // @ts-ignore
-          content: new Text( localeInfoModule[ locale ].localizedName, {
-            font: PreferencesDialog.CONTENT_FONT
-          } ),
-          minWidth: minButtonWidth + 15, // Why is UK button a bit bigger?
-          maxWidth: minButtonWidth,
-
-          tandem: Tandem.OPT_OUT
-        } );
+        // const toggleButtonProperty = new BooleanProperty( false );
+        // toggleButtonProperty.link( ( isPressed: boolean ) => {
+        //   if ( isPressed ) {
+        //     localeProperty.value = locale;
+        //   }
+        // } );
+        // localeProperty.link( pressedLocale => {
+        //   toggleButtonProperty.value = pressedLocale === locale;
+        // } );
+        //
+        // return new BooleanRectangularStickyToggleButton( toggleButtonProperty, {
+        //
+        //   // @ts-ignore
+        //   content: new Text( localeInfoModule[ locale ].localizedName, {
+        //     font: PreferencesDialog.CONTENT_FONT
+        //   } ),
+        //   minWidth: minButtonWidth + 15, // Why is UK button a bit bigger?
+        //   maxWidth: minButtonWidth,
+        //
+        //   tandem: Tandem.OPT_OUT
+        // } );
 
         // A prototype displaying clickable text for each locale
         // // @ts-ignore
-        // return new Text( localeInfoModule[ locale ].localizedName, {
-        //   font: PreferencesDialog.CONTENT_FONT,
-        //   cursor: 'pointer',
-        //   inputListeners: [
-        //     new FireListener( {
-        //       fire: () => {
-        //         localeProperty.value = locale;
-        //       },
-        //       tandem: Tandem.OPT_OUT
-        //     } )
-        //   ]
-        // } );
-      } )
+        return new Text( localeInfoModule[ locale ].localizedName, {
+          font: PreferencesDialog.CONTENT_FONT,
+          cursor: 'pointer',
+          inputListeners: [
+            new FireListener( {
+              fire: () => {
+                localeProperty.value = locale;
+              },
+              tandem: Tandem.OPT_OUT
+            } )
+          ]
+        } );
+      } );
+    }
+
+    // A prototype where we show all languages in grid managed by a Carousel so that there aren't too many items
+    // displayed at one time
+    const chunckedLocaleItems = _.chunk( createInteractiveLocales(), 5 );
+    const carouselItems = chunckedLocaleItems.map( localeItem => {
+      return new GridBox( {
+        xMargin: 10,
+        yMargin: 3,
+        xAlign: 'center',
+        autoRows: 5,
+        children: [ ...localeItem ]
+      } );
     } );
+    const carousel = new Carousel( carouselItems );
+    const content = carousel;
+
+    // A prototype where we show all languages in a grid of clickable text or buttons
+    // const content = new GridBox( {
+    //   xMargin: 10,
+    //   yMargin: 3,
+    //   xAlign: 'center',
+    //   autoRows: 20,
+    //
+    //   children: createInteractiveLocales()
+    // } );
 
     super( content, {
       title: new Text( 'Language', { font: PreferencesDialog.TITLE_FONT } ),
