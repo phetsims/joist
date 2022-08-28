25f87a6e70a02305719037d4078f12c37c9d1f2a
Sam Reid <reids@colorado.edu>
2022-08-28 09:10:43 -0600

    Add keyboard shortcuts to cycle through locales, see https://github.com/phetsims/chipper/issues/1316

--- js/localeProperty.ts
+++ js/localeProperty.ts
@@ -10,11 +10,41 @@ import StringProperty from '../../axon/js/StringProperty.js';
 import Tandem from '../../tandem/js/Tandem.js';
 import joist from './joist.js';
 
+const locales = Object.keys( phet.chipper.strings ).sort();
+
 const localeProperty = new StringProperty( phet.chipper.locale || 'en', {
   tandem: Tandem.GENERAL_MODEL.createTandem( 'localeProperty' ),
   phetioFeatured: true,
-  validValues: Object.keys( phet.chipper.strings ).sort()
+  validValues: locales
 } );
+
+// ?stringTest=keyboardShortcutLocaleSwitcher adds keyboard shortcuts for cycling
+// through the available locales
+if ( phet.chipper.queryParameters.stringTest === 'keyboardShortcutLocaleSwitcher' ) {
+
+  // Support qwerty and dvorak.  u (back) and i (forward) on the physical keyboard
+  const forwardKeys = [ 'c', 'i' ];
+  const backwardKeys = [ 'g', 'u' ];
+
+  document.addEventListener( 'keydown', ( event: KeyboardEvent ) => {
+
+    const bump = ( delta: number ) => {
+      const index = locales.indexOf( localeProperty.value );
+      const nextIndex = ( index + delta + locales.length ) % locales.length;
+      localeProperty.value = locales[ nextIndex ];
+
+      // Indicate the new locale on the console
+      console.log( localeProperty.value );
+    };
+
+    if ( event.ctrlKey && forwardKeys.includes( event.key ) ) {
+      bump( +1 );
+    }
+    else if ( event.ctrlKey && backwardKeys.includes( event.key ) ) {
+      bump( -1 );
+    }
+  } );
+}
 
 joist.register( 'localeProperty', localeProperty );
 
