6c6750f47fb15732ec31e239850606ac742ccf0f
pixelzoom <cmalley@pixelzoom.com>
2022-10-07 12:31:57 -0600

    add a link to https://github.com/phetsims/joist/issues/861 in doc

--- js/TModel.ts
+++ js/TModel.ts
@@ -1,7 +1,8 @@
 // Copyright 2022, University of Colorado Boulder
 
 /**
- * The model type for Sim.ts must have a reset function and if it has a step function, it must have this signature:
+ * The model type for Sim.ts must have a reset function and if it has a step function, it must have this signature.
+ * See https://github.com/phetsims/joist/issues/861 for the origins and design history of TModel.
  *
  * @author Sam Reid (PhET Interactive Simulations)
  */
