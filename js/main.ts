79d038edd7006f0e297331f9f215e15c8693a35c
Jonathan Olson <jonathan.olson@colorado.edu>
2022-10-27 10:45:10 -0600

    Updating phet-lib with main common code references.

---
+++ js/main.ts
@@ -0,0 +1,11 @@
+// Copyright 2022, University of Colorado Boulder
+
+import joist from './joist.js';
+import './Helper.js';
+import './JoistButton.js';
+import './KebabMenuIcon.js';
+import './MemoryMonitor.js';
+import './Profiler.js';
+import './ScreenView.js';
+
+export default joist;
