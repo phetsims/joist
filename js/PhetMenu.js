f043e6ce7112f78517646751689e42489dc4375b
Jonathan Olson <jonathan.olson@colorado.edu>
2022-11-25 14:50:09 -0700

    Respecting allowLinks, see https://github.com/phetsims/joist/issues/830

--- js/PhetMenu.js
+++ js/PhetMenu.js
@@ -259,8 +259,10 @@ define( function( require ) {
         callback: function() {
           if ( allowPopups ) {
             // Open locale-specific PhET home page. If there is no website translation for locale, fallback will be handled by server. See joist#97.
-            var phetWindow = window.open( 'http://phet.colorado.edu/' + sim.locale, '_blank' );
-            phetWindow && phetWindow.focus();
+            if ( !window.phet || !phet.chipper || !phet.chipper.queryParameters || phet.chipper.queryParameters.allowLinks ) {
+              var phetWindow = window.open( 'http://phet.colorado.edu/' + sim.locale, '_blank' );
+              phetWindow && phetWindow.focus();
+            }
           }
         }
       },
@@ -326,8 +328,10 @@ define( function( require ) {
                     '&dependencies=' + encodeURIComponent( JSON.stringify( dependenciesCopy ) );
 
           if ( allowPopups ) {
-            var reportWindow = window.open( url, '_blank' );
-            reportWindow && reportWindow.focus();
+            if ( !window.phet || !phet.chipper || !phet.chipper.queryParameters || phet.chipper.queryParameters.allowLinks ) {
+              var reportWindow = window.open( url, '_blank' );
+              reportWindow && reportWindow.focus();
+            }
           }
         },
         tandem: tandem.createTandem( 'reportAProblemButton' )
