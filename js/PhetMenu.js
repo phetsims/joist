b2c01e31b923794fd120f4e7348972c11f9476ee
Jonathan Olson <jonathan.olson@colorado.edu>
2022-11-25 21:56:37 -0700

    Allow screenshots with allowLinks=false, see https://github.com/phetsims/joist/issues/830

--- js/PhetMenu.js
+++ js/PhetMenu.js
@@ -273,7 +273,7 @@ class PhetMenu extends Node {
             }
           }
           else if ( !phet.chipper.isFuzzEnabled() ) {
-            openPopup( dataURL );
+            openPopup( dataURL, true );
           }
         },
         options: {
