c918994b761c0d552b32c265064d418fca0bb1e1
Sam Reid <reids@colorado.edu>
2023-02-01 13:25:55 -0700

    Check availableRuntimeLocales when determining isLocaleValid, see https://github.com/phetsims/beers-law-lab/issues/320
    (cherry picked from commit 4889cfb3ae2b4a5ab9cdfdfc948a4b3660140cc5)

--- js/i18n/localeProperty.ts
+++ js/i18n/localeProperty.ts
@@ -21,8 +21,8 @@ const locales = Object.keys( phet.chipper.strings ).sort() as Locale[];
 
 
 // Start only with a valid locale, see https://github.com/phetsims/phet-io/issues/1882
-const isLocaleValid = ( locale?: Locale ) => {
-  return locale && localeInfoModule[ locale ];
+const isLocaleValid = ( locale?: Locale ): boolean => {
+  return !!( locale && availableRuntimeLocales.includes( locale ) );
 };
 
 // We might use a partial locale (e.g. 'en' instead of 'en_US'), so grab this if it exists. It might be the same as
