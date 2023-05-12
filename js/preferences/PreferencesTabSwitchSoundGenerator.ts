b14ad37888d5298e15e3df665f463c299ed21329
phet-dev <phetadmin@gmail.com>
2023-05-12 03:58:46 -0600

    update copyright dates from daily grunt work

--- js/preferences/PreferencesTabSwitchSoundGenerator.ts
+++ js/preferences/PreferencesTabSwitchSoundGenerator.ts
@@ -1,4 +1,4 @@
-// Copyright 2022, University of Colorado Boulder
+// Copyright 2022-2023, University of Colorado Boulder
 
 import EnumerationProperty from '../../../axon/js/EnumerationProperty.js';
 import SoundClip, { SoundClipOptions } from '../../../tambo/js/sound-generators/SoundClip.js';
