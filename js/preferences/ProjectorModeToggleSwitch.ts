bfce1613cc9658e8b8a85855da1da624f5581333
phet-dev <phetadmin@gmail.com>
2023-06-03 03:30:29 -0600

    update copyright dates from daily grunt work

--- js/preferences/ProjectorModeToggleSwitch.ts
+++ js/preferences/ProjectorModeToggleSwitch.ts
@@ -1,4 +1,4 @@
-// Copyright 2022, University of Colorado Boulder
+// Copyright 2022-2023, University of Colorado Boulder
 
 /**
  * A toggle switch for "Projector Mode". Changes the color scheme for better contrast on projectors. The sim must have
