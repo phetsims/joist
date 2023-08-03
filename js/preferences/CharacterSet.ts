d7f8e01598e8e865e61e2884642102398939300e
phet-dev <phetadmin@gmail.com>
2023-08-03 03:32:49 -0600

    update copyright dates from daily grunt work

--- js/preferences/CharacterSet.ts
+++ js/preferences/CharacterSet.ts
@@ -1,4 +1,4 @@
-// Copyright 2022, University of Colorado Boulder
+// Copyright 2022-2023, University of Colorado Boulder
 
 /**
  * A base class for CharacterSets used in representing region and culture preferences
