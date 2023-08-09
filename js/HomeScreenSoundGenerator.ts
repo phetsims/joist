829aaf98dc18f73cea93452f4186a446b456230d
phet-dev <phetadmin@gmail.com>
2023-08-09 03:32:48 -0600

    update copyright dates from daily grunt work

--- js/HomeScreenSoundGenerator.ts
+++ js/HomeScreenSoundGenerator.ts
@@ -1,4 +1,4 @@
-// Copyright 2020-2022, University of Colorado Boulder
+// Copyright 2020-2023, University of Colorado Boulder
 
 /**
  * HomeScreenSoundGenerator is responsible for generating sounds that are associated with the home screen, such as the
