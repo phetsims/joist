81fef8ce8141d6f9fbe49b12ec65a9834869bab3
phet-dev <phetadmin@gmail.com>
2023-08-16 03:33:51 -0600

    update copyright dates from daily grunt work

--- js/thirdPartySupport/LegendsOfLearningSupport.ts
+++ js/thirdPartySupport/LegendsOfLearningSupport.ts
@@ -1,4 +1,4 @@
-// Copyright 2017-2022, University of Colorado Boulder
+// Copyright 2017-2023, University of Colorado Boulder
 
 /**
  * Support for Legends of Learning platform. Sends init message after sim is constructed and supports pause/resume.
