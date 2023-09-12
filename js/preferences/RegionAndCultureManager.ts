5c6ee5a977e002bbd284fb33d3fcacf02a46afd2
phet-dev <phetadmin@gmail.com>
2023-09-12 03:15:48 -0600

    update copyright dates from daily grunt work

--- js/preferences/RegionAndCultureManager.ts
+++ js/preferences/RegionAndCultureManager.ts
@@ -1,4 +1,4 @@
-// Copyright 2022-2023, University of Colorado Boulder
+// Copyright 2023, University of Colorado Boulder
 
 /**
  * A container managing global Properties for simulation region and culture. Contains Properties for characteristics
