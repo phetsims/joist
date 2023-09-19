d58ab696ef8a6590d50dd31dfd74b3c1291ec140
Marla Schulz <marlaa.schulz@gmail.com>
2023-09-19 08:57:51 -0600

    Remove commented out code, see: https://github.com/phetsims/center-and-variability/issues/531

--- js/preferences/RegionAndCultureManager.ts
+++ js/preferences/RegionAndCultureManager.ts
@@ -28,6 +28,4 @@ export default class RegionAndCultureManager {
   }
 }
 
-// const regionAndCultureManager = new RegionAndCultureManager();
-
 joist.register( 'RegionAndCultureManager', RegionAndCultureManager );
