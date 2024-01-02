b969001ffb2c34f420d7495187fc6f2a6067081f
phet-dev <phetadmin@gmail.com>
2024-01-02 03:16:47 -0700

    update copyright dates from daily grunt work

--- js/Helper.ts
+++ js/Helper.ts
@@ -1,4 +1,4 @@
-// Copyright 2022-2023, University of Colorado Boulder
+// Copyright 2022-2024, University of Colorado Boulder
 /**
  * Some in-simulation utilities designed to help designers and developers
  *
