67ef9341e28f0068cd41af214015c8a1494c78db
phet-dev <phetadmin@gmail.com>
2024-01-06 03:15:39 -0700

    update copyright dates from daily grunt work

--- js/preferences/PreferencesStorage.ts
+++ js/preferences/PreferencesStorage.ts
@@ -1,4 +1,4 @@
-// Copyright 2021-2022, University of Colorado Boulder
+// Copyright 2021-2024, University of Colorado Boulder
 
 /**
  * Singleton that can save and load a Property's value to localStorage, and keep it in sync for the next runtime.
