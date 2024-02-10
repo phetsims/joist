55930fbd1787868915d895e23645a2afedd9bc4e
phet-dev <phetadmin@gmail.com>
2024-02-10 03:28:17 -0700

    update copyright dates from daily grunt work

--- js/i18n/localeProperty.ts
+++ js/i18n/localeProperty.ts
@@ -1,4 +1,4 @@
-// Copyright 2022-2023, University of Colorado Boulder
+// Copyright 2022-2024, University of Colorado Boulder
 
 /**
  * A universal locale Property that is accessible independently of the running Sim instance.
