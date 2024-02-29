d9982c46593a41976a3a2e072e759fdf810f40b5
phet-dev <phetadmin@gmail.com>
2024-02-29 03:19:22 -0700

    update copyright dates from daily grunt work

--- js/preferences/RegionAndCultureComboBox.ts
+++ js/preferences/RegionAndCultureComboBox.ts
@@ -1,4 +1,4 @@
-// Copyright 2022-2023, University of Colorado Boulder
+// Copyright 2022-2024, University of Colorado Boulder
 
 /**
  * A ComboBox that lets you change a character or portrayal in a simulation to match a particular culture or region.
