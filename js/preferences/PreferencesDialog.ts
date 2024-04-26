b94d701c92cab77cd4cf554f333a7e0701c5b01b
phet-dev <phetadmin@gmail.com>
2024-04-26 03:19:01 -0600

    update copyright dates from daily grunt work

--- js/preferences/PreferencesDialog.ts
+++ js/preferences/PreferencesDialog.ts
@@ -1,4 +1,4 @@
-// Copyright 2021-2023, University of Colorado Boulder
+// Copyright 2021-2024, University of Colorado Boulder
 
 /**
  * Dialog with preferences to enable or disable various features for the simulation. Groups of preferences are
