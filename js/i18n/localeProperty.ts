42fd962729dd8f392f2bc7636b3c4e253bfe7557
Jonathan Olson <jonathan.olson@colorado.edu>
2024-06-12 13:00:43 -0600

    Replacing localeProperty validValues with availableRuntimeLocales, adding duplicated initialize-globals logic, see https://github.com/phetsims/joist/issues/970

--- js/i18n/localeProperty.ts
+++ js/i18n/localeProperty.ts
@@ -12,53 +12,164 @@ import { globalKeyStateTracker, KeyboardUtils } from '../../../scenery/js/import
 import Tandem from '../../../tandem/js/Tandem.js';
 import StringIO from '../../../tandem/js/types/StringIO.js';
 import joist from '../joist.js';
+import { ReadOnlyPropertyState } from '../../../axon/js/ReadOnlyProperty.js';
 
 const FALLBACK_LOCALE = 'en';
 
 export type Locale = string;
+
+assert && assert( phet.chipper.locale, 'phet.chipper.locale global expected' );
+assert && assert( phet.chipper.localeData, 'phet.chipper.localeData global expected' );
+assert && assert( phet.chipper.strings, 'phet.chipper.strings global expected' );
+assert && assert( phet.chipper.queryParameters.locale, 'should exist with a default' );
+
+/**
+ * Given a locale based on the supported query parameter schema, map it to the 2 or 5 char locale code (key in localeData).
+ */
+const remapLocale = ( locale: string ) => {
+  assert && assert( locale );
+  assert && assert( phet.chipper.localeData );
+
+  const inputValueLocale = locale;
+
+  if ( locale.length < 5 ) {
+    locale = locale.toLowerCase();
+  }
+  else {
+    locale = locale.replace( /-/, '_' );
+
+    const parts = locale.split( '_' );
+    if ( parts.length === 2 ) {
+      locale = parts[ 0 ].toLowerCase() + '_' + parts[ 1 ].toUpperCase();
+    }
+  }
+
+  if ( locale.length === 3 ) {
+    for ( const candidateLocale of Object.keys( phet.chipper.localeData ) ) {
+      if ( phet.chipper.localeData[ candidateLocale ].locale3 === locale ) {
+        locale = candidateLocale;
+        break;
+      }
+    }
+  }
+
+  // Permissive patterns for locale query parameter patterns.
+  // We don't want to show a query parameter warning if it matches these patterns, EVEN if it is not a valid locale
+  // in localeData, see https://github.com/phetsims/qa/issues/1085#issuecomment-2111105235.
+  const pairRegex = /^[a-zA-Z]{2}$/;
+  const tripleRegex = /^[a-zA-Z]{3}$/;
+  const doublePairRegex = /^[a-zA-Z]{2}[_-][a-zA-Z]{2}$/;
+
+  // Sanity checks for verifying localeData (so hopefully we don't commit bad data to localeData).
+  if ( assert ) {
+    for ( const locale of Object.keys( phet.chipper.localeData ) ) {
+      // Check the locale itself
+      assert( pairRegex.test( locale ) || doublePairRegex.test( locale ), `Invalid locale format: ${locale}` );
+
+      // Check locale3 (if it exists)
+      if ( phet.chipper.localeData[ locale ].locale3 ) {
+        assert( tripleRegex.test( phet.chipper.localeData[ locale ].locale3 ), `Invalid locale3 format: ${phet.chipper.localeData[ locale ].locale3}` );
+      }
+
+      // Check fallbackLocales (if it exists)
+      if ( phet.chipper.localeData[ locale ].fallbackLocales ) {
+        for ( const fallbackLocale of phet.chipper.localeData[ locale ].fallbackLocales ) {
+          assert( phet.chipper.localeData[ fallbackLocale ] );
+        }
+      }
+    }
+  }
+
+  if ( !phet.chipper.localeData[ locale ] ) {
+    const badLocale = inputValueLocale;
+
+    if ( !pairRegex.test( badLocale ) && !tripleRegex.test( badLocale ) && !doublePairRegex.test( badLocale ) ) {
+      assert && assert( false, 'invalid locale:', inputValueLocale );
+    }
+
+    locale = FALLBACK_LOCALE;
+  }
+
+  return locale;
+};
+
+/**
+ * Get the "most" valid locale, see https://github.com/phetsims/phet-io/issues/1882
+ *  As part of https://github.com/phetsims/joist/issues/963, this as changed. We check a specific fallback order based
+ *  on the locale. In general, it will usually try a prefix for xx_XX style locales, e.g. 'ar_SA' would try 'ar_SA', 'ar', 'en'
+ *  NOTE: If the locale doesn't actually have any strings: THAT IS OK! Our string system will use the appropriate
+ *  fallback strings.
+ */
+const getValidRuntimeLocale = ( locale: string ) => {
+  assert && assert( locale );
+  assert && assert( phet.chipper.localeData );
+  assert && assert( phet.chipper.strings );
+
+  const possibleLocales = [
+    locale,
+    ...( phet.chipper.localeData[ locale ]?.fallbackLocales ?? [] ),
+    FALLBACK_LOCALE
+  ];
+
+  const availableLocale = possibleLocales.find( possibleLocale => !!phet.chipper.strings[ possibleLocale ] );
+  assert && assert( availableLocale, 'no fallback found for ', locale );
+  return availableLocale;
+};
+
+// We will need to check for locale validity (once we have localeData loaded, if running unbuilt), and potentially
+// either fall back to `en`, or remap from 3-character locales to our locale keys. This overwrites phet.chipper.locale.
+// Used when setting locale through JOIST/localeProperty also. Default to the query parameter instead of
+// chipper.locale because we overwrite that value, and may run this function multiple times during the startup
+// sequence (in unbuilt mode).
+const checkAndRemapLocale = ( locale: string ) => {
+
+  // We need both to proceed. Provided as a global, so we can call it from load-unbuilt-strings
+  // (IF initialize-globals loads first). Also handle the unbuilt mode case where we have phet.chipper.strings
+  // exists but no translations have loaded yet.
+  if ( !phet.chipper.localeData || !phet.chipper.strings?.hasOwnProperty( FALLBACK_LOCALE ) || !locale ) {
+    return locale;
+  }
+
+  const remappedLocale = remapLocale( locale );
+  const finalLocale = getValidRuntimeLocale( remappedLocale );
+
+  phet.chipper.locale = finalLocale; // NOTE: this will change with every setting of JOIST/localeProperty
+  return finalLocale;
+};
 
 // All available locales for the runtime
 export const availableRuntimeLocales = _.sortBy( Object.keys( phet.chipper.strings ), locale => {
   return StringUtils.localeToLocalizedName( locale ).toLowerCase();
 } );
 
-// Start only with a valid locale, see https://github.com/phetsims/phet-io/issues/1882
-const isLocaleValid = ( locale?: Locale ): boolean => {
-  return !!( locale && availableRuntimeLocales.includes( locale ) );
-};
+export class LocaleProperty extends Property<Locale> {
+  public readonly availableRuntimeLocales: Locale[] = availableRuntimeLocales;
 
-// Get the "most" valid locale, see https://github.com/phetsims/phet-io/issues/1882
-// As part of https://github.com/phetsims/joist/issues/963, this as changed. We check a specific fallback order based
-// on the locale. In general, it will usually try a prefix for xx_XX style locales, e.g. 'ar_SA' would try 'ar_SA', 'ar', 'en'
-// NOTE: If the locale doesn't actually have any strings: THAT IS OK! Our string system will use the appropriate
-// fallback strings.
-const validInitialLocale = [
-  phet.chipper.locale,
-  ...( phet.chipper.localeData[ phet.chipper.locale ]?.fallbackLocales ?? [] ),
-  FALLBACK_LOCALE
-].find( isLocaleValid );
-
-// Just in case we had an invalid locale, remap phet.chipper.locale to the "corrected" value
-phet.chipper.locale = validInitialLocale;
-
-class LocaleProperty extends Property<Locale> {
   protected override unguardedSet( value: Locale ): void {
-    if ( availableRuntimeLocales.includes( value ) ) {
-      super.unguardedSet( value );
-    }
-    else {
-      assert && assert( false, 'Unsupported locale: ' + value );
+    // NOTE: updates phet.chipper.locale as a side-effect
+    super.unguardedSet( checkAndRemapLocale( value ) );
+  }
 
-      // Do not try to set if the value was invalid
-    }
+  // This improves the PhET-iO Studio interface, by giving available values, without triggering validation if you want
+  // to use the more general locale schema (three digit/case-insensitive/etc).
+  protected override toStateObject<StateType>(): ReadOnlyPropertyState<StateType> {
+    const parentObject = super.toStateObject<StateType>();
+
+    // Provide via validValues without forcing validation assertions if a different value is set.
+    parentObject.validValues = this.availableRuntimeLocales as StateType[];
+    return parentObject;
+  }
+
+  protected override applyState<StateType>( stateObject: ReadOnlyPropertyState<StateType> ): void {
+    stateObject.validValues = null; // TODO: this should be removed in https://github.com/phetsims/axon/issues/453
+    super.applyState( stateObject );
   }
 }
 
-const localeProperty = new LocaleProperty( validInitialLocale, {
+const localeProperty = new LocaleProperty( phet.chipper.locale, {
   tandem: Tandem.GENERAL_MODEL.createTandem( 'localeProperty' ),
   phetioFeatured: true,
   phetioValueType: StringIO,
-  validValues: availableRuntimeLocales,
   phetioDocumentation: 'Specifies language currently displayed in the simulation'
 } );
 
