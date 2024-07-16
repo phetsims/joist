e8cbf03b1a1d81eac3caa1f34a4837033013a513
Jesse <jesse.greenberg@colorado.edu>
2024-07-16 13:03:48 -0400

    localization labels and deescription are translatable, see https://github.com/phetsims/joist/issues/976

--- js/JoistStrings.ts
+++ js/JoistStrings.ts
@@ -124,6 +124,10 @@ type StringsType = {
       };
       'localization': {
         'titleStringProperty': LocalizedStringProperty;
+        'languageSelection': {
+          'titleStringProperty': LocalizedStringProperty;
+          'descriptionStringProperty': LocalizedStringProperty;
+        };
         'regionAndCulture': {
           'titleStringProperty': LocalizedStringProperty;
           'descriptionStringProperty': LocalizedStringProperty;
@@ -251,8 +255,6 @@ type StringsType = {
         };
         'localization': {
           'languageSelection': {
-            'labelStringProperty': LocalizedStringProperty;
-            'descriptionStringProperty': LocalizedStringProperty;
             'languageChangeResponsePatternStringProperty': LocalizedStringProperty;
           }
         }
