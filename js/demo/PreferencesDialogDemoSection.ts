b42d275ded24e12d19ed6353aa3ba69982828082
phet-dev <phetadmin@gmail.com>
2024-11-09 03:25:44 -0700

    update copyright dates from daily grunt work

--- js/demo/PreferencesDialogDemoSection.ts
+++ js/demo/PreferencesDialogDemoSection.ts
@@ -1,4 +1,4 @@
-// Copyright 2022, University of Colorado Boulder
+// Copyright 2022-2024, University of Colorado Boulder
 
 /**
  * A demo for the different features and components for a preferences dialog.
--- js/preferences/AudioPreferencesPanel.ts
+++ js/preferences/AudioPreferencesPanel.ts
@@ -1,4 +1,4 @@
-// Copyright 2021-2023, University of Colorado Boulder
+// Copyright 2021-2024, University of Colorado Boulder
 
 /**
  * The panel for the PreferencesDialog containing preferences related to audio.
