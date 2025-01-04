f1fc36387bf12b14ce1e0b682d4393e5d0822a82
phet-dev <phetadmin@gmail.com>
2025-01-04 03:50:30 -0700

    update copyright dates from daily grunt work

--- js/ScreenSummaryContent.ts
+++ js/ScreenSummaryContent.ts
@@ -1,4 +1,4 @@
-// Copyright 2024, University of Colorado Boulder
+// Copyright 2024-2025, University of Colorado Boulder
 
 /**
  * Parallel DOM content for a screen summary of a ScreenView. This is a screen specific summary that is available
