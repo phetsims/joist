81e6830dedf1282b9e4e3110260882c2cbb9283a
Michael Kauzmann <michael.kauzmann@colorado.edu>
2025-03-05 15:04:17 -0700

    fix lint, https://github.com/phetsims/special-ops/issues/291

--- js/Sim.ts
+++ js/Sim.ts
@@ -73,7 +73,6 @@ import StringIO from '../../tandem/js/types/StringIO.js';
 import { Locale } from './i18n/localeProperty.js';
 
 // constants
-const PROGRESS_BAR_WIDTH = 273;
 const SUPPORTS_GESTURE_DESCRIPTION = platform.android || platform.mobileSafari;
 
 // globals
