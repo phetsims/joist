a47c7a5bd0a74c7658a8b5a736394fe5d0957dd6
Sam Reid <reids@colorado.edu>
2025-10-03 10:29:07 -0600

    Remove unused imports from generateFluentTypes, see https://github.com/phetsims/chipper/issues/1636

--- js/JoistFluent.ts
+++ js/JoistFluent.ts
@@ -5,12 +5,8 @@
 /* eslint-disable */
 /* @formatter:off */
 
-import { TReadOnlyProperty } from '../../axon/js/TReadOnlyProperty.js';
-import type { FluentVariable } from '../../chipper/js/browser/FluentPattern.js';
-import FluentPattern from '../../chipper/js/browser/FluentPattern.js';
-import FluentContainer from '../../chipper/js/browser/FluentContainer.js';
 import FluentConstant from '../../chipper/js/browser/FluentConstant.js';
-import FluentComment from '../../chipper/js/browser/FluentComment.js';
+import FluentContainer from '../../chipper/js/browser/FluentContainer.js';
 import joist from './joist.js';
 import JoistStrings from './JoistStrings.js';
 
