ee20b25dfdfc5ef39aaa3fb6fd7e07f7921b639b
Jonathan Olson <jonathan.olson@colorado.edu>
2026-03-13 22:23:11 -0600

    Navbar visual line added for PhET brand with the CC BY-NC changes, see phetsims/special-ops#318

--- js/HomeScreenView.js
+++ js/HomeScreenView.js
@@ -26,6 +26,7 @@ define( function( require ) {
   var joist = require( 'JOIST/joist' );
   var Util = require( 'DOT/Util' );
   var PhetColorScheme = require( 'SCENERY_PHET/PhetColorScheme' );
+  var Line = require( 'SCENERY/nodes/Line' );
 
   // phet-io modules
   var TScreenButton = require( 'ifphetio!PHET_IO/types/joist/TScreenButton' );
@@ -243,6 +244,21 @@ define( function( require ) {
     // this button. See https://github.com/phetsims/joist/issues/304.
     this.phetButton = new PhetButton( sim, homeScreenFillProperty, homeScreenTextFillProperty, tandem.createTandem( 'phetButton' ) );
     this.addChild( this.phetButton );
+
+    // Thin line to the left of the PhET button (which won't interact with layout).
+    // See https://github.com/phetsims/special-ops/issues/318, added when CC BY-NC license was added.
+    this.phetButtonLine = new Line( {
+      stroke: 'white',
+      lineWidth: 0.7,
+      opacity: 0.7
+    } );
+    if ( phet.chipper.brand === 'phet' ) {
+      this.phetButton.addChild( this.phetButtonLine );
+    }
+
+    this.phetButtonLine.y1 = this.phetButton.localBounds.top + 2;
+    this.phetButtonLine.y2 = this.phetButton.localBounds.bottom - 4;
+    this.phetButtonLine.centerX = this.phetButton.localBounds.left - 13 / 2;
 
     if ( options.warningNode ) {
       var warningNode = options.warningNode;
