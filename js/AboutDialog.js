d57cc13dded69f8ae04aa191037456cbe1973af0
Jonathan Olson <jonathan.olson@colorado.edu>
2026-03-18 16:30:27 -0600

    Copyright end year fix, see https://github.com/phetsims/joist/issues/962

--- js/AboutDialog.js
+++ js/AboutDialog.js
@@ -51,7 +51,13 @@ define( function( require ) {
     children.push( new Text( name, { font: new PhetFont( 28 ), maxWidth: MAX_WIDTH } ) );
     children.push( new Text( StringUtils.format( versionPatternString, version ), { font: new PhetFont( 20 ), maxWidth: MAX_WIDTH } ) );
     if ( phet.chipper.buildTimestamp ) {
-      children.push( new Text( phet.chipper.buildTimestamp, { font: new PhetFont( 13 ), maxWidth: MAX_WIDTH } ) );
+      var year = phet.chipper.buildTimestamp ? // defined for built versions
+                 phet.chipper.buildTimestamp.split( '-' )[0] : // e.g. "2017-04-20 19:04:59 UTC" -> "2017"
+                 new Date().getFullYear(); // in requirejs mode
+
+      var copyright = StringUtils.format( Brand.copyright.replace( '{{year}}', '{0}' ), year );
+
+      children.push( new Text( copyright, { font: new PhetFont( 13 ), maxWidth: MAX_WIDTH } ) );
     }
 
     if ( UpdateCheck.areUpdatesChecked ) {
