8b817b55f6bae7ca45698860d1504f1cbd752984
Jonathan Olson <jonathan.olson@colorado.edu>
2026-03-18 10:43:38 -0600

    Removing duplicate links, see https://github.com/phetsims/special-ops/issues/319

--- js/AboutDialog.ts
+++ js/AboutDialog.ts
@@ -228,7 +228,7 @@ export default class AboutDialog extends Dialog {
 
         // If links are allowed, use hyperlinks. Otherwise, just output the URL. This doesn't need to be internationalized.
         const stringProperty = new DerivedStringProperty( [ allowLinksProperty, link.textStringProperty ], ( allowLinks, linkText ) => {
-          return allowLinks ? `<a href="{{url}}">${linkText}</a>` : `${linkText}: ${link.url}`;
+          return allowLinks ? `<a href="{{url}}"><u>${linkText}</u></a>` : `${linkText}: ${link.url}`;
         } );
 
         // This is PhET-iO instrumented because it is a keyboard navigation focusable element.
@@ -237,45 +237,6 @@ export default class AboutDialog extends Dialog {
           font: new PhetFont( NOMINAL_FONT_SIZE )
         } ) );
       }
-
-      // Show the links in a separate VBox so they will have the same MAX_WIDTH and hence the same font size.
-      const linksParent = new VBox( {
-        spacing: 6,
-        align: 'left',
-        children: linksChildren, maxWidth: MAX_WIDTH
-      } );
-      children.push( linksParent );
-
-      // Create new links whenever the locale changes, for an up to date translator URL.
-      localeProperty.link( locale => {
-        linksParent.children.forEach( child => child.dispose() );
-
-        const links = Brand.getLinks( packageJSON.name, locale );
-        const linksChildren: Node[] = [];
-
-        linksChildren.push( new VStrut( 15 - 6 ) );
-
-        for ( let i = 0; i < links.length; i++ ) {
-          const link = links[ i ];
-
-          // If links are allowed, use hyperlinks. Otherwise, just output the URL. This doesn't need to be internationalized.
-          const stringProperty = new DerivedStringProperty( [ allowLinksProperty, link.textStringProperty ], ( allowLinks, linkText ) => {
-            return allowLinks ? `<a href="{{url}}"><u>${linkText}</u></a>` : `${linkText}: ${link.url}`;
-          } );
-
-          // This is PhET-iO instrumented because it is a keyboard navigation focusable element.
-          const richText = new RichText( stringProperty, {
-            links: { url: link.url }, // RichText must fill in URL for link
-            font: new PhetFont( NOMINAL_FONT_SIZE )
-          } );
-          richText.disposeEmitter.addListener( () => {
-            stringProperty.dispose();
-          } );
-          linksChildren.push( richText );
-        }
-
-        linksParent.children = linksChildren;
-      } );
     }
 
     const content = new VBox( {
