160598080ac90606255ef3a0cb7808a4e5bbb50a
Jonathan Olson <jonathan.olson@colorado.edu>
2026-03-19 09:55:50 -0600

    Removing accessibleHeading before it exists, see https://github.com/phetsims/special-ops/issues/319

--- js/AboutDialog.ts
+++ js/AboutDialog.ts
@@ -185,10 +185,7 @@ export default class AboutDialog extends Dialog {
       } );
 
       licenseChildren.push( new VoicingText( JoistStrings.license.titleStringProperty, {
-        font: new PhetFont( { size: NOMINAL_FONT_SIZE, weight: 'bold' } ),
-
-        accessibleHeading: JoistStrings.credits.titleStringProperty,
-        accessibleParagraph: null
+        font: new PhetFont( { size: NOMINAL_FONT_SIZE, weight: 'bold' } )
       } ) );
 
       licenseChildren.push( new VoicingRichText( licenseStringProperty, {
