82dfc16aa63182a2cf9b02d2281186d0829fcb20
Jonathan Olson <jonathan.olson@colorado.edu>
2026-03-26 10:26:24 -0600

    AllowLinks patch for bottom AboutDialog links, see https://github.com/phetsims/special-ops/issues/325 and https://github.com/phetsims/special-ops/issues/318

--- js/AboutDialog.js
+++ js/AboutDialog.js
@@ -148,8 +148,9 @@ define( function( require ) {
 
     var licenseChildren = [];
 
+    var allowLinks = phet.chipper.queryParameters.allowLinks;
     if ( Brand.license ) {
-      var licenseString = ( !phet.chipper.queryParameters.allowLinks && Brand.licenseWithoutLinks ) ? Brand.licenseWithoutLinks : Brand.license;
+      var licenseString = ( !allowLinks && Brand.licenseWithoutLinks ) ? Brand.licenseWithoutLinks : Brand.license;
 
       licenseChildren.push( new Text( licenseTitleString, {
         font: new PhetFont( { size: 16, weight: 'bold' } )
@@ -185,7 +186,7 @@ define( function( require ) {
       children.push( new VStrut( 15 - 6 ) );
       for ( var i = 0; i < links.length; i++ ) {
         var link = links[ i ];
-        children.push( new RichText( '<a href="{{url}}"><u>' + link.text + '</u></a>', {
+        children.push( new RichText( allowLinks ? '<a href="{{url}}"><u>' + link.text + '</u></a>' : link.text + ' ' + link.url, {
           links: { url: link.url }, // RichText must fill in URL for link
           font: new PhetFont( 14 ),
           maxWidth: MAX_WIDTH,
